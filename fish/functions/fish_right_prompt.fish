4e78826690ae3bda4c485492d1cfdff22b20f66c
Justus Perlwitz <mail@justusperlwitz.com>
2015-07-05 00:37:08 +0200

    ADD: Cool braces around right prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -3,7 +3,7 @@ function fish_right_prompt
   if math "$last_status > 0" >/dev/null
     set_color red
   end
-  echo -n "$last_status "
+  echo -n "[$last_status] "
   set_color normal
   date "+%h %d"
 end
