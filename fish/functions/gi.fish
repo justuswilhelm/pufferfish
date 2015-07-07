3bec4343ff1e6fcac928d050791d4ca056fb7132
Justus Perlwitz <mail@justusperlwitz.com>
2015-07-07 21:12:57 +0200

    do not use array notation in gi

--- fish/functions/gi.fish
+++ fish/functions/gi.fish
@@ -1,6 +1,7 @@
 # Better git init
 function gi
-  set files ("README.md" "CHANGELOG.md" "LICENSE" "CONTRIBUTING.md")
+  set files README.md CHANGELOG.md LICENSE CONTRIBUTING.md
   touch $files
   git add $files
+  git commit -m "ADD: Initial commit"
 end
