b4cc4ac2f1fc56a63cc8ce3f83ce19de77307909
Justus Perlwitz <mail@justusperlwitz.com>
2015-07-28 00:49:03 +0200

    ADD: Pimp'd right prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,9 +1,13 @@
 function fish_right_prompt
   set last_status "$status"
+  set_color green
   if math "$last_status > 0" >/dev/null
     set_color red
   end
-  echo -n "[$last_status] "
+  echo -n "[$last_status]"
+  set_color blue
+  date "+%H%M"
+  set_color magenta
+  date "+%h%d"
   set_color normal
-  date "+%h %d"
 end
