bc34e9768e50babb0cdd08af18ab590c6e0a9239
Justus Perlwitz <mail@justusperlwitz.com>
2015-11-21 23:32:54 +0100

    ADD: Local bin folder

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,9 @@ set PATH $PATH /usr/local/heroku/bin
 # MacTex
 set PATH $PATH /usr/local/texlive/2014basic/bin/x86_64-darwin/
 
+# Local Bin
+set PATH $PATH ~/bin
+
 if [ -f /usr/local/share/autojump/autojump.fish ]
   source /usr/local/share/autojump/autojump.fish
 end
