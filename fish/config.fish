e0df5fb6aaaac06811eb50c1c577648a1b810596
Justus Perlwitz <mail@justusperlwitz.com>
2016-01-09 20:12:42 +0100

    REF: put fish aliases into config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,29 @@
-source ~/.config/fish/alias.fish
+# Git
+# ===
+alias changelog "git commit CHANGELOG.md -m 'DOC: Update Changelog'"
+alias ga "git add"
+alias gap "git add -p"
+alias gc "git checkout"
+alias gd "git diff"
+alias gdc "git diff --cached"
+alias gl "git log"
+alias gm "git commit"
+alias gma "git commit --amend"
+alias gs "git status"
+
+# Python
+# ======
+alias pip_install "pip install -r requirements.txt"
+alias s_env "source env/bin/activate.fish"
+
+# Other
+# =====
+alias hosts "sudo vim /etc/hosts"
+alias latexmk "latexmk -pdf -pvc"
+alias ssh-x "ssh -c arcfour,blowfish-cbc -XC"
+alias vim "nvim"
+alias dotfiles "pushd; cd ~/.dotfiles"
+
 # Homebrew
 set PATH $PATH /usr/local/bin:/usr/local/sbin
 
