ca0d745bd8eaf25a44a04d52a21753782ee59787
Justus Perlwitz <mail@justusperlwitz.com>
2016-01-17 17:39:24 +0100

    FIX: cd override

--- fish/functions/cd.fish
+++ fish/functions/cd.fish
@@ -2,8 +2,8 @@ function cd
   builtin cd $argv
   if not tty -s
     return 0
-  else if [ -e env/bin/activate ]
-    s_env
+  else if [ -e env/bin/activate.fish ]
+    source env/bin/activate.fish
   end
   return 0
 end
