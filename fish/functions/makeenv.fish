40338db083648fd054f6fe2db0142591d215ea37
Justus Perlwitz <mail@justusperlwitz.com>
2016-01-23 12:32:21 +0100

    better virtualenv helpers in fish

--- fish/functions/makeenv.fish
+++ fish/functions/makeenv.fish
@@ -1,4 +1,16 @@
 function makeenv
-  virtualenv env -ppython3
-  s_env
+  set ENV_DIR "env"
+  set REQUIREMENTS "requirements.txt"
+
+  if [ -d "$ENV_DIR" ]
+    echo "Path $ENV_DIR already exists, aborting."
+    return 1
+  else
+    virtualenv "$ENV_DIR" -ppython3
+    source "$ENV_DIR/bin/activate.fish"
+
+    if [ -e "$REQUIREMENTS" ]
+      pip install -r "$REQUIREMENTS"
+    end
+  end
 end
