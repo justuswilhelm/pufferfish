b105f68bc2e6ba9e714237ddf6543903c37f881a
Justus Perlwitz <mail@justusperlwitz.com>
2016-02-03 10:03:19 +0000

    MOD: condense right prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,10 +1,11 @@
 function fish_right_prompt
     set last_status "$status"
-    set_color green
     if math "$last_status > 0" > /dev/null
         set_color red
+    else
+        set_color green
     end
-    echo -n "[$last_status]"
+    echo -n "$last_status"
     set_color blue
     date "+%H%M"
     set_color magenta
