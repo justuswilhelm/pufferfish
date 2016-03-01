f9fd26ba72297910538f7b98639e07c62eaf9d9f
Justus Perlwitz <mail@justusperlwitz.com>
2016-03-01 16:36:35 +0100

    Make fish prompt more compact, show unstaged files

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,24 +2,31 @@ function git_status
     if git rev-parse --is-inside-work-tree 1> /dev/null 2>& 1
         if not git diff --quiet
             set_color red
-            echo -n "✘ "
+            echo -n "✘"
             set_color normal
         end
         if not git diff --quiet --cached
             set_color yellow
-            echo -n "✷ "
+            echo -n "✔︎"
             set_color normal
         end
+        begin
+          set -l IFS ""
+          if test -n ""(git ls-files --others --exclude-standard)""
+            set_color green
+            echo -n "☆"
+          end
+        end
         set_color blue
-        echo -n ""(git name-rev --name-only --no-undefined --always HEAD) ""
+        echo -n ""(git name-rev --name-only --no-undefined --always HEAD)""
         set_color normal
     end
 end
 
 function fish_prompt
     set_color yellow
-    echo -n "$PWD "
+    echo -n "$PWD"
     set_color normal
     git_status
-    echo -n "\$ "
+    echo -n "\$"
 end
