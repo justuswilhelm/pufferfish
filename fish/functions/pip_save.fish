52416a839f71f0d367c1e9f7b434f3dbcd5867cb
Justus Perlwitz <mail@justusperlwitz.com>
2016-03-15 23:38:23 +0100

    Add cool pip save helper

---
+++ fish/functions/pip_save.fish
@@ -0,0 +1,9 @@
+function pip_save
+    set -l reqs "requirements.txt"
+    pip install $argv[1]
+    set freeze (pip freeze | grep $argv[1])
+    set tfile (mktemp)
+    cp $reqs $tfile
+    echo $freeze >> $tfile
+    uniq $tfile | sort -o $reqs
+end
