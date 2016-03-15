910903eb8658731537f4dbb927322a5f81febf68
Justus Perlwitz <mail@justusperlwitz.com>
2016-03-15 23:41:11 +0100

    Properly indent fish files

--- fish/functions/makeenv.fish
+++ fish/functions/makeenv.fish
@@ -1,14 +1,14 @@
 function makeenv
-	set ENV_DIR "env"
-  set REQUIREMENTS "requirements.txt"
-  if [ -d "$ENV_DIR" ]
-      echo "Path $ENV_DIR already exists, aborting."
-      return 1
-  else
-      virtualenv "$ENV_DIR" -ppython3 --system-site-packages
-      source "$ENV_DIR/bin/activate.fish"
-      if [ -e "$REQUIREMENTS" ]
-          pip install -r "$REQUIREMENTS"
-      end
-  end
+    set ENV_DIR "env"
+    set REQUIREMENTS "requirements.txt"
+    if [ -d "$ENV_DIR" ]
+        echo "Path $ENV_DIR already exists, aborting."
+        return 1
+    else
+        virtualenv "$ENV_DIR" -ppython3 --system-site-packages
+        source "$ENV_DIR/bin/activate.fish"
+        if [ -e "$REQUIREMENTS" ]
+            pip install -r "$REQUIREMENTS"
+        end
+    end
 end
