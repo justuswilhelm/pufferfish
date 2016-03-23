9019adf9800bcceced9faff43a90ba43fcfe40fa
Justus Perlwitz <mail@justusperlwitz.com>
2016-03-23 22:06:35 +0100

    Clean up code

--- fish/functions/config_python.fish
+++ fish/functions/config_python.fish
@@ -1,13 +1,13 @@
 function config_python
-  if set -q VIRTUAL_ENV
-    echo "Storing \$VIRTUAL_ENV"
-    set -l _VIRTUAL_ENV $VIRTUAL_ENV
-    set -e VIRTUAL_ENV
-  end
-  pip3 install -r $DOTFILES/python/requirements.txt
-  if set -q _VIRTUAL_ENV
-    echo "Restoring \$VIRTUAL_ENV"
-    set -l VIRTUAL_ENV $_VIRTUAL_ENV
-  end
+    if set -q VIRTUAL_ENV
+        echo "Storing \$VIRTUAL_ENV"
+        set -l _VIRTUAL_ENV $VIRTUAL_ENV
+        set -e VIRTUAL_ENV
+    end
+    pip3 install -r $DOTFILES/python/requirements.txt
+    if set -q _VIRTUAL_ENV
+        echo "Restoring \$VIRTUAL_ENV"
+        set -l VIRTUAL_ENV $_VIRTUAL_ENV
+    end
 end
 
