dd673503327afdaa42d36a88a1b55993f94f49ef
Justus Perlwitz <mail@justusperlwitz.com>
2016-04-20 16:48:25 +0200

    fish: clean up indent

--- fish/functions/weather.fish
+++ fish/functions/weather.fish
@@ -1,7 +1,7 @@
 function weather
-	while true
-clear
-curl http://wttr.in/$argv[1]
-sleep 60
-end
+    while true
+        clear
+        curl "http://wttr.in/$argv[1]"
+        sleep 60
+    end
 end
