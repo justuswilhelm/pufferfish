b1db460d69da63b248701a320b3a85a23f1faa4f
Justus Perlwitz <mail@justusperlwitz.com>
2016-07-08 14:12:06 +0200

    Config: add emacs conf

--- fish/functions/config_symlinks.fish
+++ fish/functions/config_symlinks.fish
@@ -1,11 +1,11 @@
 function config_symlinks
-    echo $XDG_CONFIG_HOME
+	echo $XDG_CONFIG_HOME
     mkdir -pv $XDG_CONFIG_HOME
     rm -rf "$XDG_CONFIG_HOME/fish"
     for path in "fish" "nvim"
         ln -sfv "$DOTFILES/$path" $XDG_CONFIG_HOME
     end
-    for path in "latexmkrc" "brewfile" "tmux.conf" "isort.cfg"
+    for path in "latexmkrc" "brewfile" "tmux.conf" "isort.cfg" "emacs"
         ln -fv "$DOTFILES/misc/$path" $HOME/.$path
     end
     ln -sfv "$DOTFILES/bin" "$HOME"
