4f7e5928d08cdd641a028aa9bba60b94dfe47fd3
Justus Perlwitz <mail@justusperlwitz.com>
2016-07-21 21:30:26 +0200

    fish: use abbrs

--- fish/config.fish
+++ fish/config.fish
@@ -14,37 +14,34 @@ end
 
 # Git
 # ===
-alias ga "git add"
-alias gaa "git add -A"
-alias gap "git add -p"
-alias gc "git checkout"
-alias gd "git diff"
-alias gdc "git diff --cached"
-alias gl "git log"
-alias gm "git commit"
-alias gme "git merge"
-alias gmm "git commit -m"
-alias gr "git remote"
-alias grs "git reset --soft HEAD~1"
-alias gs "git status"
-alias gfa "git fetch --all"
+abbr -a ga git add
+abbr -a ga git add
+abbr -a gap git add -p
+abbr -a gc git checkout
+abbr -a gd git diff
+abbr -a gdc git diff --cached
+abbr -a gl git log
+abbr -a gm git commit
+abbr -a gmm git commit -m
+abbr -a gr git remote
+abbr -a gs git status
+abbr -a gfa git fetch --all
 
 # Python
 # ======
-alias pi "pip install -r requirements.txt"
-alias se "source env/bin/activate.fish"
-alias pm "python -m"
-alias doctest "python -m doctest"
+abbr -a pi pip install
+abbr -a pir pip install -r requirements.txt
+abbr -a se source env/bin/activate.fish
+abbr -a pm python -m
+abbr -a doctest python -m doctest
 
 # Other
 # =====
-alias bf "nvim ~/.brewfile"
-alias pf "nvim $DOTFILES/python/requirements.txt"
-alias p2f "nvim $DOTFILES/python/requirements2.txt"
-alias bi "brew install"
-alias binf "brew info"
-alias o "open"
-alias suggest "history | cut -f1,2 -d' ' | uniq -c | sort -r | head -n10"
-alias t "tmux"
-alias m "tmuxinator"
-alias vim "nvim"
+abbr -a bf nvim ~/.brewfile
+abbr -a pf nvim $DOTFILES/python/requirements.txt
+abbr -a p2f nvim $DOTFILES/python/requirements2.txt
+abbr -a bi brew install
+abbr -a binf brew info
+abbr -a t tmux
+abbr -a m tmuxinator
+abbr -a vim nvim
