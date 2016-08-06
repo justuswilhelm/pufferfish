be35882c8f0c54bf7e999bc455116c611f818941
Justus Perlwitz <mail@justusperlwitz.com>
2016-08-06 10:13:14 +0200

    fish: sort abbrs lexicographically

--- fish/config.fish
+++ fish/config.fish
@@ -20,31 +20,31 @@ abbr -a gap git add -p
 abbr -a gc git checkout
 abbr -a gd git diff
 abbr -a gdc git diff --cached
+abbr -a gfa git fetch --all
 abbr -a gl git log
 abbr -a gm git commit
 abbr -a gmm git commit -m
+abbr -a gpf git push --force
+abbr -a gpr git pull --rebase
 abbr -a gr git remote
 abbr -a gs git status
-abbr -a gfa git fetch --all
-abbr -a gpr git pull --rebase
-abbr -a gpf git push --force
 
 # Python
 # ======
+abbr -a doctest python -m doctest
 abbr -a pi pip install
 abbr -a pir pip install -r requirements.txt
-abbr -a se source env/bin/activate.fish
 abbr -a pm python -m
-abbr -a doctest python -m doctest
+abbr -a se source env/bin/activate.fish
 
 # Other
 # =====
 abbr -a bf nvim ~/.brewfile
-abbr -a pf nvim $DOTFILES/python/requirements.txt
-abbr -a p2f nvim $DOTFILES/python/requirements2.txt
 abbr -a bi brew install
 abbr -a binf brew info
-abbr -a t tmux
 abbr -a m tmuxinator
-abbr -a vim nvim
 abbr -a mt make test
+abbr -a p2f nvim $DOTFILES/python/requirements2.txt
+abbr -a pf nvim $DOTFILES/python/requirements.txt
+abbr -a t tmux
+abbr -a vim nvim
