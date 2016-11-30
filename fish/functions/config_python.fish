1fe9bb0261808763a20272573d4682166a744f93
Justus Perlwitz <mail@justusperlwitz.com>
2016-11-30 12:00:47 +0100

    Python: Fix config script

--- fish/functions/config_python.fish
+++ fish/functions/config_python.fish
@@ -5,12 +5,13 @@ function config_python
         set -l _VIRTUAL_ENV $VIRTUAL_ENV
         set -e VIRTUAL_ENV
     end
+    set -l IGNORE 'pip|setuptools|pkg_resources'
     if contains $argv "clean"
-        pip2 freeze | grep -v -E 'vboxapi|git' | xargs pip2 uninstall -y
-        pip3 freeze | grep -v -E 'vboxapi|git' | xargs pip3 uninstall -y
+        pip2 list | cut -f1 -d' ' | grep -v -E $IGNORE | xargs pip2 uninstall -y
+        pip3 list | cut -f1 -d' ' | grep -v -E $IGNORE | xargs pip3 uninstall -y
     end
-    pip2 install -r $DOTFILES/python/requirements2.txt
-    pip3 install -r $DOTFILES/python/requirements.txt
+    pip2 install -r $DOTFILES/python/requirements2.txt --upgrade
+    pip3 install -r $DOTFILES/python/requirements.txt --upgrade
     if set -q _VIRTUAL_ENV
         echo "Restoring \$VIRTUAL_ENV"
         set -l VIRTUAL_ENV $_VIRTUAL_ENV
