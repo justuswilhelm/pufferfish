7e208f70004443c70e63d7d542ef2c2092759eff
Justus Perlwitz <mail@justusperlwitz.com>
2016-12-26 15:10:59 +0100

    Brew: Add cabal, stack and ghc

--- fish/config.fish
+++ fish/config.fish
@@ -13,7 +13,7 @@ set -x GOPATH $HOME/go
 set fish_user_paths #{staged_path}/#{token}/bin
 set -x MANPATH #{staged_path}/#{token}/help/man /usr/local/share/man /usr/share/man /opt/x11/share/man
 
-for p in "/usr/local/bin" "/usr/local/heroku/bin" "$HOME/bin"
+for p in "/usr/local/bin" "/usr/local/heroku/bin" "$HOME/bin" "$HOME/.cabal/bin"
     set PATH $PATH $p
 end
 
