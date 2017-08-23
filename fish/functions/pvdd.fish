6cd533183b6d82d8ce903621c290e1b57c929e3a
Justus <mail@justusperlwitz.com>
2017-08-23 12:26:28 +0200

    Fish/Functions: Make pvdd compatible with linux

--- fish/functions/pvdd.fish
+++ fish/functions/pvdd.fish
@@ -1,3 +1,8 @@
 function pvdd
-    pv "$argv[1]" | sudo dd "of=$argv[2]" bs=1m
+	if is_darwin
+        set -l block 1m
+    else
+        set -l block 1M
+    end
+	pv "$argv[1]" | sudo dd "of=$argv[2]" bs=1M
 end
