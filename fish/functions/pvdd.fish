167b951f116cbf56a6d562ca1f77b347a58ed81e
Justus <mail@justusperlwitz.com>
2017-10-17 09:10:55 +0200

    Fish/pvdd: Fix bs flag

--- fish/functions/pvdd.fish
+++ fish/functions/pvdd.fish
@@ -1,8 +1,8 @@
 function pvdd
 	if is_darwin
-        set -l block 1m
+        set -l DD_FLAG bs=4m
     else
-        set -l block 1M
+        set -l DD_FLAG bs=4M
     end
-	pv "$argv[1]" | sudo dd "of=$argv[2]" bs=1M
+	pv "$argv[1]" | sudo dd "of=$argv[2]" $DD_FLAG
 end
