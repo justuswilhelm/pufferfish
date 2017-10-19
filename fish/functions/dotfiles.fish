aab3031d5fc6c6c97d07d483247f733870e791ff
Justus <mail@justusperlwitz.com>
2017-10-20 01:10:28 +0200

    Fish/Dotfiles: Make dotfiles friendly and green

--- fish/functions/dotfiles.fish
+++ fish/functions/dotfiles.fish
@@ -1,4 +1,15 @@
 function dotfiles
     pushd $DOTFILES
-    git pull --rebase
+    if not git pull --rebase
+        echo
+        echo "---"
+        echo
+        set_color green
+        echo "Howdy dotfiles friend!"
+        set_color normal
+        echo
+        echo "I could not pull the latest changes from the repository."
+        echo "You might want to stash your changes and try running 'dotfiles'"
+        echo "again."
+    end
 end
