7d03baab15c20a83a486a2d65bb7f26e04502ba5
Justus <mail@justusperlwitz.com>
2017-10-21 17:45:49 +0200

    Fish: Reindent functions

--- fish/functions/pvdd.fish
+++ fish/functions/pvdd.fish
@@ -1,8 +1,8 @@
 function pvdd
-	if is_darwin
+    if is_darwin
         set -l DD_FLAG bs=4m
     else
         set -l DD_FLAG bs=4M
     end
-	pv "$argv[1]" | sudo dd "of=$argv[2]" $DD_FLAG
+    pv "$argv[1]" | sudo dd "of=$argv[2]" $DD_FLAG
 end
