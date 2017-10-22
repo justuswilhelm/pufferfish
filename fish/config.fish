7f3b17fb0242d98bc54916aab9dd02e9d6f5a509
Justus Perlwitz <mail@justusperlwitz.com>
2017-10-22 23:36:26 +0200

    Fish: Reorganize shortcuts

--- fish/config.fish
+++ fish/config.fish
@@ -19,22 +19,18 @@ add_to_path "$HOME/go/bin"
 
 # Abbreviations
 # =============
+# Clean abbreviations
+set -e fish_user_abbreviations
 
 # Fish
 # ----
-abbr -a reload 'exec fish'
+# Reload fish sesseion. Useful if config.fish has changed.
+abbr -a reload exec fish
 
 # Files
 # -----
+# Make it easier to ls a folder by saving one character.
 abbr -a l ls
-
-# Crypto
-# ------
-abbr pbcopy <~/.ssh/id_rsa.pub
-
-# Network
-# -------
-abbr -a random_mac 'openssl rand -hex 6 | sed \'s/\(..\)/\1:/g; s/.$//\''
 
 # Git
 # ---
@@ -46,32 +42,28 @@ abbr -a gd git diff
 abbr -a gdc git diff --cached
 abbr -a gds git diff --shortstat --cached
 abbr -a gfa git fetch --all
+abbr -a gi 'git init; and git commit --allow-empty -m "Initial commit"'
 abbr -a gl git log
 abbr -a gm git commit
 abbr -a gma git commit --amend
 abbr -a gmm git commit -m
 abbr -a gp git push
 abbr -a gpf git push --force
-abbr -a gpr 'git pull --rebase'
+abbr -a gpr git pull --rebase
 abbr -a gpu 'git push --set-upstream origin (git rev-parse --abbrev-ref HEAD)'
 abbr -a gr git remote
 abbr -a gra git rebase --abort
 abbr -a grc git rebase --continue
 abbr -a gri git rebase -i
 abbr -a gro 'git fetch --all; and git rebase origin/master'
-abbr -a groh 'git reset origin/master --hard'
+abbr -a groh git reset origin/master --hard
 abbr -a gs git status
-abbr -a gi 'git init; and git commit --allow-empty -m "Initial commit"'
 
 # Python
 # ------
-abbr -a doctest python -m doctest
 abbr -a pi pip install
 abbr -a pir pip install -r requirements.txt
-abbr -a pm python -m
 abbr -a s source env/bin/activate.fish
-abbr -a p2f nvim $DOTFILES/python/requirements2.txt
-abbr -a pf nvim $DOTFILES/python/requirements.txt
 
 # tmux
 # ----
@@ -81,7 +73,3 @@ abbr -a ta tmux a
 # Neovim
 # -----
 abbr -a e nvim
-
-# Crontab
-# -------
-abbr -a cr env VIM_CRONTAB=true /usr/bin/env crontab
---
+++ fish/functions/pubkey.fish
@@ -0,0 +1,6 @@
+function pubkey
+    set -l pubkey_location "$HOME/.ssh/id_rsa.pub"
+    echo "Copying ssh public key from $pubkey_location"
+    pbcopy < $pubkey_location
+    or xclip < $pubkey_location
+end
