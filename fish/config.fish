5ea4a974838037d4f330c80b94c354930a8d32fa
Justus Perlwitz <mail@justusperlwitz.com>
2017-10-23 02:11:33 +0200

    Fish: Document abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -25,54 +25,84 @@ add_to_path "$HOME/go/bin"
 # Clean abbreviations
 set -e fish_user_abbreviations
 
-# Fish
-# ----
-# Reload fish sesseion. Useful if config.fish has changed.
+# Fish abbreviations
+# ------------------
+# Reload fish session. Useful if config.fish has changed.
 abbr -a reload exec fish
 
-# Files
-# -----
+# File abbreviations
+# ------------------
 # Make it easier to ls a folder by saving one character.
 abbr -a l ls
 
-# Git
-# ---
+# Git abbreviations
+# -----------------
+# Stage changed files in git index
 abbr -a ga git add
+# Stage changes in files in patch mode
 abbr -a gap git add -p
+# Check out a branch or file
 abbr -a gc git checkout
+# Check out a new branch
 abbr -a gcb git checkout -b
+# Show the current diff for unstaged changes
 abbr -a gd git diff
+# Show the current diff for staged changes
 abbr -a gdc git diff --cached
+# Show commit statistics for staged changes
 abbr -a gds git diff --shortstat --cached
+# Fetch from all remote branches
 abbr -a gfa git fetch --all
+# Initialize an empty repository
 abbr -a gi 'git init; and git commit --allow-empty -m "Initial commit"'
+# Show the git log
 abbr -a gl git log
+# Commit the current staged changes
 abbr -a gm git commit
+# Amend to the previous commit
 abbr -a gma git commit --amend
+# Commit the current staged changes with a message
 abbr -a gmm git commit -m
+# Push to remote
 abbr -a gp git push
+# Push to remote and force update (potentially dangerous)
 abbr -a gpf git push --force
+# Bring current branch up to date with a rebase
 abbr -a gpr git pull --rebase
+# Push and create branch if it has not yet been created remotely
 abbr -a gpu 'git push --set-upstream origin (git rev-parse --abbrev-ref HEAD)'
+# Show remote repositories
 abbr -a gr git remote
+# Abort a rebase
 abbr -a gra git rebase --abort
+# Continue with the next rebase step
 abbr -a grc git rebase --continue
+# Perform interactive rebase
 abbr -a gri git rebase -i
+# Rebase current branch from origin master
 abbr -a gro 'git fetch --all; and git rebase origin/master'
+# Reset current branch to origin master (dangerous)
 abbr -a groh git reset origin/master --hard
+# Show current status
 abbr -a gs git status
 
-# Python
-# ------
+# Python abbreviations
+# --------------------
+# Install a package with pip
 abbr -a pi pip install
+# Install requirements with pip based on requirements.txt
 abbr -a pir pip install -r requirements.txt
+# Activate virtual env in env/bin/activate.fish
 abbr -a s source env/bin/activate.fish
 
-# tmux
-# ----
+# tmux abbreviations
+# ------------------
+# Start tmux
 abbr -a t tmux
+# Attach to existing session
 abbr -a ta tmux a
 
-# Neovim
-# -----
+# Neovim abbreviations
+# --------------------
+# Start neovim
 abbr -a e nvim
