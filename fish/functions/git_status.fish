f99f22df72a357ba92b765d980d05de9d2896df3
Justus Perlwitz <mail@justusperlwitz.com>
2017-12-19 06:20:31 +0100

    Fish: Add timeout to git_status

--- fish/functions/git_status.fish
+++ fish/functions/git_status.fish
@@ -1,18 +1,18 @@
 function git_status -d "Show git status"
-    if git rev-parse --is-inside-work-tree 1>/dev/null 2>&1
-        if not git diff --quiet
+    if timelimit -t 0.1 git rev-parse --is-inside-work-tree 1>/dev/null 2>&1
+        if not timelimit -t 0.1 git diff --quiet
             set_color red
             echo -n "!"
             set_color normal
         end
-        if not git diff --quiet --cached
+        if not timelimit -t 0.1 git diff --quiet --cached
             set_color yellow
             echo -n "¡"
             set_color normal
         end
         begin
             set -l IFS ""
-            if test -n ""(git ls-files --others --exclude-standard)""
+            if test -n ""(timelimit -t 0.1 git ls-files --others --exclude-standard)""
                 set_color green
                 echo -n "+"
             end
