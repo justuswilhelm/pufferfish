5ad523be7b19eed1c83ac2256896f872d4f9ed15
Justus Perlwitz <mail@justusperlwitz.com>
2022-12-06 11:32:51 +0900

    Fish: Fix fish_add_path

--- fish/functions/fish_add_path.fish
+++ fish/functions/fish_add_path.fish
@@ -1,6 +1,6 @@
 function fish_add_path --description "Add paths to the PATH"
-    if contains $argv $PATH
+    if contains $argv $fish_user_paths
         return
     end
-    set PATH $argv $PATH
+    set fish_user_paths $argv $fish_user_paths
 end
