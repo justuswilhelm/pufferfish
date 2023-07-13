e804dbc260daa0e83f7c8ea5954441c39dde7395
Justus Perlwitz <hello@justus.pw>
2023-07-14 00:54:21 +0900

    Neovim: Shuffle around config
    This adds nvim-tree and org mode

--- nvim/new-init.lua
+++ nvim/new-init.lua
@@ -1,3 +1,35 @@
+require("nvim-tree").setup({
+    sort_by = "case_sensitive",
+    view = {
+        width = 30,
+    },
+    renderer = {
+        group_empty = true,
+        icons = {
+            webdev_colors = false,
+            glyphs = {
+                default = ">",
+                symlink = "%",
+                bookmark = "#",
+                modified = "●",
+                folder = {
+                    arrow_closed = "v",
+                    arrow_open = ">",
+                    default = ">",
+                    open = ">",
+                    empty = "c",
+                    empty_open = ">",
+                    symlink = "v",
+                    symlink_open = ">",
+                },
+            },
+        },
+    },
+    filters = {
+        dotfiles = true,
+    },
+})
+
 require("other-nvim").setup({
     mappings = {
         {
@@ -27,3 +59,21 @@ vim.api.nvim_set_keymap("n", "<leader>oo", "<cmd>:Other<CR>", { noremap = true, 
 vim.api.nvim_set_keymap("n", "<leader>os", "<cmd>:OtherSplit<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>ov", "<cmd>:OtherVSplit<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>oc", "<cmd>:OtherClear<CR>", { noremap = true, silent = true })
+-- Load custom treesitter grammar for org filetype
+require('orgmode').setup_ts_grammar()
+
+-- Treesitter configuration
+require('nvim-treesitter.configs').setup {
+    highlight = {
+        enable = true,
+        -- Required for spellcheck, some LaTex highlights and
+        -- code block highlights that do not have ts grammar
+        additional_vim_regex_highlighting = {'org'},
+    },
+    ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
+
+require('orgmode').setup({
+})
+
+require("nvim-surround").setup()
