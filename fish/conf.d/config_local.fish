14c5bbe5322fe950db0f705b210ab1de1d41f0f0
Justus Perlwitz <hello@justus.pw>
2023-09-13 13:13:01 +0900

    Fish: Set timewarrior DB location

--- fish/conf.d/config_local.fish
+++ fish/conf.d/config_local.fish
@@ -2,6 +2,9 @@ if is_darwin
     # Macports
     fish_add_path --global /opt/local/bin
 
+    # Timewarrior
+    # set -x TIMEWARRIORDB "$HOME/.config/timewarrior"
+
     # We use timeout from coreutils
     set -x TIMEOUT_CMD gtimeout
 end
