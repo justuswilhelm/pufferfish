43b9a66b24d6c5c4217d313aecd5803503dd0f72
Justus Perlwitz <hello@justus.pw>
2023-12-20 09:51:32 +0900

    git-annex in justusperlwitz:~/.dotfiles

--- fish/conf.d/darwin.fish
+++ fish/conf.d/darwin.fish
@@ -2,8 +2,17 @@ if is_darwin
     # Macports
     fish_add_path --global /opt/local/bin
 
+    # Git annex
+    # --append is important, otherwise it collides with the system git
+    fish_add_path --append --global /Applications/Free/git-annex.app/Contents/MacOS
+
+    # Rust
+    set -x RUSTUP_HOME "$HOME/.local/share/rustup"
+    set -x CARGO_HOME "$HOME/.local/share/cargo"
+    fish_add_path --global "$HOME/.local/share/cargo/bin"
+
     # Timewarrior
-    # set -x TIMEWARRIORDB "$HOME/.config/timewarrior"
+    set -x TIMEWARRIORDB "$HOME/.config/timewarrior"
 
     # We use timeout from coreutils
     set -x TIMEOUT_CMD gtimeout
