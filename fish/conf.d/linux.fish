986498e70aacf1e178120bc6fbb9880ad0dc28ec
Justus Perlwitz <hello@justus.pw>
2023-12-20 10:41:35 +0900

    Fish: Make SSH_AUTH_SOCK checking more robust

--- fish/conf.d/linux.fish
+++ fish/conf.d/linux.fish
@@ -1,7 +1,10 @@
 if is_linux
     # for ssh-agent
-    set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
-    if test ! -e "$SSH_AUTH_SOCK"
-        echo "Could not find $SSH_AUTH_SOCK"
+    # Need XDG_RUNTIME_DIR, which is not present over SSH
+    if [ -n "$XDG_RUNTIME_DIR" ]
+        set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+        if [ ! -e "$SSH_AUTH_SOCK" ]
+            echo "Could not find $SSH_AUTH_SOCK" >&2
+        end
     end
 end
