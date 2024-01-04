c49b999b6523952bf91b9a204593afa4ebcfd325
Justus Perlwitz <hello@justus.pw>
2024-01-04 21:09:30 +0900

    Nix: Leave it up to nix to create borgmatic helper

--- bin/borgmatic_timestamp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://apple.stackexchange.com/a/406097
-set -e
-/run/current-system/sw/bin/borgmatic create prune \
-  --verbosity 1 \
-  --log-file-verbosity 1 \
-  --log-file "$HOME/Library/Logs/borgmatic/borgmatic.$(date -Iseconds).log" \
-  2> >(/run/current-system/sw/bin/ts -m '[%Y-%m-%d %H:%M:%S] -' 1>&2) \
-  1> >(/run/current-system/sw/bin/ts -m '[%Y-%m-%d %H:%M:%S] -')
