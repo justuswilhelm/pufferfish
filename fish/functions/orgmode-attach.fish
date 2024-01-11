48f9f66938a795d9a8922fd42ada8f671290d1b9
Justus Perlwitz <hello@justus.pw>
2024-01-11 14:23:49 +0900

    Fish: Turn orgmode-attach into a fish function

--- fish/functions/orgmode-attach.fish
+++ fish/functions/orgmode-attach.fish
@@ -1,43 +1,38 @@
-#!/bin/bash
-# Add orgmode paths here
-ORGMODE_PATHS=""
+function orgmode-attach
+    # Add orgmode paths here
+    set ORGMODE_PATHS ""
 
-if ! path=$(echo "$ORGMODE_PATHS" | fzf)
-then
-    echo "No path was specified"
-    exit 1
-fi
+    if ! set path (echo "$ORGMODE_PATHS" | fzf)
+        echo "No path was specified"
+        return 1
+    end
 
-session="orgmode-$(basename "$path" | tr "A-Z " "a-z-")"
+    set session "orgmode-"(basename "$path" | tr "A-Z " "a-z-")
 
-echo "Assuming session name $session"
+    echo "Assuming session name $session"
 
-if tmux has-session -t "$session"
-then
-    echo "Attaching to session"
-    if exec tmux attach -t "$session"
-    then
-        echo "Couldn't attach"
-        exit 1
-    fi
-else
-    echo "Session does not exist yet"
-fi
+    if tmux has-session -t "$session"
+        echo "Attaching to session"
+        tsa "$session:0"; or return 1
+        return
+    else
+        echo "Session does not exist yet"
+    end
 
-cd "$path"
+    cd "$path"
 
-# -d prevents attaching
-if tmux new-session -d -s "$session" -n "nvim"
-then
-    echo "Created session"
-else
-    echo "Couldn't create session"
-    exit 1
-fi
+    # -d prevents attaching
+    if tmux new-session -d -s "$session" -n "nvim"
+        echo "Created session"
+    else
+        echo "Couldn't create session"
+        return 1
+    end
 
-tmux send-keys -t "$session:0" "nvim" C-m || exit 1
+    tmux send-keys -t "$session:0" "nvim" C-m || exit 1
 
-tmux new-window -t "$session" -n "files" || exit 1
-tmux send-keys -t "$session:1" "ls" C-m || exit 1
+    tmux new-window -t "$session" -n "files" || exit 1
+    tmux send-keys -t "$session:1" "ls" C-m || exit 1
 
-tmux attach -t "$session:0"
+    tsa "$session:0"
+end
