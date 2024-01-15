82e47ddcaeab82b396622a1b0c80d1eda18faa54
Justus Perlwitz <hello@justus.pw>
2024-01-15 15:05:15 +0900

    Fish: Fix cwd bug in orgmode-attach
    Would not cd into target dir correctly

--- fish/functions/orgmode-attach.fish
+++ fish/functions/orgmode-attach.fish
@@ -19,20 +19,18 @@ function orgmode-attach
         echo "Session does not exist yet"
     end
 
-    cd "$path"
-
     # -d prevents attaching
-    if tmux new-session -d -s "$session" -n "nvim"
+    if tmux new-session -c "$path" -d -s "$session" -n "nvim"
         echo "Created session"
     else
         echo "Couldn't create session"
         return 1
     end
 
-    tmux send-keys -t "$session:0" "nvim" C-m || exit 1
+    tmux send-keys -t "$session:0" "nvim" C-m || return 1
 
-    tmux new-window -t "$session" -n "files" || exit 1
-    tmux send-keys -t "$session:1" "ls" C-m || exit 1
+    tmux new-window -c "$path" -t "$session" -n "files" || return 1
+    tmux send-keys -t "$session:1" "ls" C-m || return 1
 
     tsa "$session:0"
 end
