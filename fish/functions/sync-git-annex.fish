892758586053d83fd32b99f71d5946188eca4d3b
Justus Perlwitz <hello@justus.pw>
2024-01-18 15:40:51 +0900

    Fish: Use GIT_ANNEX_PATHS in sync-git-annex

--- fish/functions/sync-git-annex.fish
+++ fish/functions/sync-git-annex.fish
@@ -1,5 +1,9 @@
 function sync-git-annex
-    for annex in "TODO"
+    if ! set -q GIT_ANNEX_PATHS
+        echo "Must define GIT_ANNEX_PATHS"
+        return 1
+    end
+    for annex in $GIT_ANNEX_PATHS
         echo "Sync $annex"
         fish -c "cd $annex && git annex sync --content"
         or return
