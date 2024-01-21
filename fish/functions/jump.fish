99de7db519dac616c73306d83a819dcfe0007b36
Justus Perlwitz <hello@justus.pw>
2024-01-21 13:05:55 +0900

    Fish: Improve jump
    If inside git repo, jump relative to root

--- fish/functions/jump.fish
+++ fish/functions/jump.fish
@@ -1,5 +1,11 @@
 function jump
-    if ! set dest (fd --type d | fzf)
+    # If we are inside a git repo we want to jump relative to current dir
+    if git rev-parse --is-inside-work-tree &>/dev/null
+        set where (git rev-parse --show-toplevel)
+    else
+        set where $PWD
+    end
+    if ! set dest (fd --type d . $where | fzf --scheme=path)
         echo "Must specify destination"
         return 1
     end
