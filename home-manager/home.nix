b78400a3bf109d51d0db50c47bf36f54ade3b9f7
Justus Perlwitz <hello@justus.pw>
2024-01-25 17:47:27 +0900

    Simplify fish config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -325,9 +325,39 @@ in {
     terminal = "screen-256color";
   };
 
-  programs.fish = {
+  programs.fish = let
+    debianConfig = ''
+    # for ssh-agent
+    # Need XDG_RUNTIME_DIR, which is not present over SSH
+    if [ -n $XDG_RUNTIME_DIR ]
+        set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+        if [ ! -e $SSH_AUTH_SOCK ]
+            echo "Could not find $SSH_AUTH_SOCK" >&2
+        end
+    end
+    '';
+  in {
     enable = true;
-    interactiveShellInit = builtins.readFile ../fish/config.fish;
+    loginShellInit = if isDebian then ''
+    # Only need to source this once
+    source /nix/var/nix/profiles/default/etc/profile.d/nix.fish
+
+    # We always want to enable wayland in moz, since we start sway through the terminal
+    set -x MOZ_ENABLE_WAYLAND 1
+
+    # If running from tty1 start sway
+    set TTY1 (tty)
+
+    if [ $TTY1 = /dev/tty1 ]
+        exec sway
+    end
+    '' else "";
+    interactiveShellInit = ''
+    # Theme
+    # =====
+    # TODO make this selenized
+    fish_config theme choose "Solarized Light"
+    ${if isDebian then debianConfig else ""}'';
     shellAbbrs = {
       # Fish abbreviations
       # ------------------
