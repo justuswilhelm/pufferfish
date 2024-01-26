c1fd84fbfb5b194fe235a68ca7a86e02e86d5fd6
Justus Perlwitz <hello@justus.pw>
2024-01-26 12:18:28 +0900

    Manage i3status from nix

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -29,7 +29,6 @@ in
     let
       debianOnly = lib.lists.optionals isDebian [
         # Compositor
-        pkgs.i3status
         # This won't load because of some OpenGL issue
         # pkgs.sway
         # Swaylock doesn't work well.
@@ -175,8 +174,6 @@ in
         (linkScript "${dotfiles}/pypoetry" "${xdgConfigHome}")
         # TODO symlink me as config file
         (linkScript "${dotfiles}/sway" "${xdgConfigHome}")
-        # TODO symlink me as config file
-        (linkScript "${dotfiles}/i3status" "${xdgConfigHome}")
         # TODO symlink me as config file
         (linkScript "${dotfiles}/x" "${xdgConfigHome}")
         # TODO symlink me as config file
@@ -534,6 +531,71 @@ in
         # Install foot-themes
         include = "${pkgs.foot.themes}/share/foot/themes/selenized-light";
         font = "Iosevka Fixed:size=11";
+      };
+    };
+  };
+
+  programs.i3status = {
+    enable = isDebian;
+    enableDefault = false;
+    general =
+      {
+        output_format = "i3bar";
+        interval = 5;
+      };
+
+    modules = {
+      "ethernet enp7s0" = {
+        settings = {
+          format_up = "enp7s0: %ip (%speed)";
+          format_down = "enp7s0: down";
+        };
+        position = 0;
+      };
+
+      "disk /" = {
+        settings = {
+          format = "/ %free";
+        };
+        position = 1;
+      };
+
+      "disk /home" = {
+        settings = {
+          format = "/home %free";
+        };
+        position = 2;
+      };
+
+      load = {
+        settings = {
+          format = "load: %1min, %5min, %15min";
+        };
+        position = 3;
+      };
+
+      memory = {
+        settings = {
+          format = "f: %free a: %available u: %used t: %total";
+          threshold_degraded = "10%";
+          format_degraded = "MEMORY: %free";
+        };
+        position = 4;
+      };
+
+      "tztime UTC" = {
+        settings = {
+          format = "UTC %Y-%m-%d %H:%M:%S";
+          timezone = "UTC";
+        };
+        position = 5;
+      };
+
+      "tztime local" = {
+        settings = {
+          format = "LCL %Y-%m-%d %H:%M:%S %Z";
+        };
+        position = 6;
       };
     };
   };
