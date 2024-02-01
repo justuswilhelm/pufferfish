73320705cd61572b45cd7ca1fb2e412cf2d8bbab
Justus Perlwitz <hello@justus.pw>
2024-02-01 14:10:06 +0900

    Turn fn into a fish function

--- fish/functions/fn.fish
+++ fish/functions/fn.fish
@@ -1,4 +1,10 @@
-#!/usr/bin/env fish
-set -x FZF_DEFAULT_COMMAND "fdfind --type f --strip-cwd-prefix --hidden --follow --exclude .git"
-set p (fzf)
-exec nnn "$p"
+function fn -d "Open nnn at folder chosen with fzf"
+    set p (
+        fd --type f \
+            --strip-cwd-prefix \
+            --hidden \
+            --follow \
+            --exclude .git |
+        fzf)
+    and nnn $p
+end
