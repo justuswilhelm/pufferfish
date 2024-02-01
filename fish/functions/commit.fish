f927bcf2f37fd5d406d4f10f0c5d705ca144bab0
Justus Perlwitz <hello@justus.pw>
2024-02-01 14:06:30 +0900

    Fix spaces in fish commit fn

--- fish/functions/commit.fish
+++ fish/functions/commit.fish
@@ -1,20 +1,21 @@
 function commit -d "Suggest git commit messages based on previous commit messages of staged files"
-    if ! set files (git diff --cached --name-only)
+    set files (git diff --cached --name-only) || begin
         echo "Nothing is staged"
-        exit 1
+        return 1
     end
+
     # Last 10 commit subjects, commented out
-    if ! set message (git log --pretty=%s -- $files | head -n10 | sed -e 's/^/# /')
+    set message (git log --pretty=%s -- $files | head -n10 | sed -e 's/^/# /') || begin
         echo "Couldn't get log for these files:
-        $files
-        "
-        exit 1
+$files
+"
+        return 1
     end
 
     git commit --edit --message "
 
-    # Commit message suggestions:
-    # ---------------------------
-    #
-    $message"
+# Commit message suggestions:
+# ---------------------------
+#
+$message"
 end
