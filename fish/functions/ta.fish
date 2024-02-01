4a1dad6baa9543b4df5c7ec78ef761019def6762
Justus Perlwitz <hello@justus.pw>
2024-02-01 13:59:39 +0900

    Turn ta into a fish function

--- fish/functions/ta.fish
+++ fish/functions/ta.fish
@@ -1,8 +1,14 @@
-#!/bin/bash
-# Prompt the user to pick an existing tmux session and connect to it
-if ! session="$(tmux ls -F "#{session_name}" | fzf)"
-then
-    echo "No session specified. Exiting."
-    exit 1
-fi
-exec tmux attach -t "$session"
+function ta -d "Prompt the user to pick an existing tmux session and connect to it"
+    if ! set sessions (tmux ls -F '#{session_name}')
+        echo "Could not get sessions"
+        return 1
+    end
+    if ! set session (printf '%s\n' $sessions | fzf)
+        echo "No session specified."
+        return 1
+    end
+    tsa $session || begin
+        echo "Couldn't attach to tmux sessions"
+        return 1
+    end
+end
