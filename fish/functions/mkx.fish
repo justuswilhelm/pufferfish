e8393ab80a705a927881b113c03e65e50bad5c83
Justus Perlwitz <hello@justus.pw>
2024-02-01 14:19:36 +0900

    Turn mkx into a fish function

--- fish/functions/mkx.fish
+++ fish/functions/mkx.fish
@@ -1,10 +1,12 @@
-#!/bin/bash
-set -e
-dest="$1"
-if [ -a "$dest" ]
-then
-    echo "$(realpath "$dest") already exists"
-    exit 1
-fi
-printf "#!/bin/bash\necho Hello World" > "$dest"
-chmod -v +x "$dest"
+function mkx -d "Create an executable at path" -a dest
+    if [ -e $dest ]
+        echo "$(realpath "$dest") already exists"
+        return 1
+    end
+    echo "#!/usr/bin/env sh
+# Maybe set -e, -o pipefail or -u
+echo Hello World
+" >$dest
+    chmod -v +x $dest
+    ls -lh $dest
+end
