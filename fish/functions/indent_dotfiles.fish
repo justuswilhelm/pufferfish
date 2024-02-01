c190ab275b1e29cd93187af2b1afc3dbe5a3132a
Justus Perlwitz <hello@justus.pw>
2024-02-01 14:25:26 +0900

    Turn indent_dotfiles into a fish function

--- fish/functions/indent_dotfiles.fish
+++ fish/functions/indent_dotfiles.fish
@@ -1,7 +1,6 @@
-#!/usr/bin/env fish
-# Indent all fish files in the fish folder
-for file in */**.fish
-  echo $file
-  fish_indent < $file > temp.fish
-  mv temp.fish $file
+function indent_dotfiles -d "Indent all fish files in the fish folder"
+    for file in (fd -e fish . $DOTFILES/fish)
+        echo "Fixing up $file"
+        fish_indent -w $file
+    end
 end
