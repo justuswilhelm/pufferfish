1ee5115d8ed21175bca9833b46ca5828c8a38a2c
Justus Perlwitz <hello@justus.pw>
2024-02-06 14:14:41 +0900

    Fix aerospace opening alacritty in new instance
    Alacritty would show up several times in Dock.
    
    The solution is to message alacritty to create a new window. alacritty
    msg does that for us. Handy.
    
    https://github.com/alacritty/alacritty/issues/6735

--- nix/home-manager/aerospace.nix
+++ nix/home-manager/aerospace.nix
@@ -1,10 +1,13 @@
 # Aerospace toml config, returns derivation, not text!
-{ lib, pkgs }: let
+{ lib, pkgs }:
+let
   source = builtins.fromTOML (builtins.readFile ../../aerospace/aerospace.toml);
   extra = {
-    mode.main.binding.alt-enter = "exec-and-forget ${pkgs.alacritty}/bin/alacritty";
+    mode.main.binding = {
+      alt-enter = "exec-and-forget ${pkgs.alacritty}/bin/alacritty msg create-window || alacritty";
+    };
   };
   merged = lib.attrsets.recursiveUpdate source extra;
   tomlFormat = pkgs.formats.toml { };
 in
-  tomlFormat.generate "aerospace.toml" merged
+tomlFormat.generate "aerospace.toml" merged
