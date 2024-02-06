327d0adbbc97f871ac165e2f61ed6cbb6686b90b
Justus Perlwitz <hello@justus.pw>
2024-02-06 18:05:35 +0900

    Rework branch-off command
    It should always rebase off of an origin

--- fish/functions/branch-off.fish
+++ fish/functions/branch-off.fish
@@ -2,36 +2,36 @@ function branch-off
     argparse 'p/parent=' 'r/remote=' no-remote -- $argv
     or return
 
+    if set -q _flag_remote
+        set remote "$_flag_remote"
+        echo "Using remote $remote"
+    else if set -q _flag_no_remote
+        echo "Assuming parent is a local branch"
+    else
+        set remote origin
+        echo "Assuming remote is $remote"
+    end
+
     if set -q _flag_parent
         set parent "$_flag_parent"
-        if set -q _flag_remote
-            set remote "$_flag_remote"
-            echo "Using remote $remote"
-        else if set -q _flag_no_remote
-            echo "Assuming parent is a local branch"
-        else
-            set remote origin
-            echo "Assuming remote is $remote"
-        end
 
         if set -q remote
-            if ! git fetch "$remote"
+            if ! git fetch $remote
                 echo "Could not fetch from $remote"
                 return 1
             end
             set upstream "$remote/$parent"
         else
-            set upstream "$parent"
+            set upstream $parent
         end
-
-        echo "Branching off $upstream"
     else
         set upstream (git branch --show-current)
-        echo "Branching off current branch $upstream"
     end
 
-    if test -n "$argv[1]"
-        set branchname "$argv[1]"
+    echo "Branching off $upstream"
+
+    if set -q argv[1]
+        set branchname $argv[1]
         echo "Using branchname $branchname"
     else if read -P "Enter new branch name: " branchname
         echo "Using $branchname as new branch name"
@@ -45,5 +45,6 @@ function branch-off
     set new_branch "$user/$date-$branchname"
 
     echo "New branch will be $new_branch"
-    git checkout -b "$new_branch" "$upstream"
+    git checkout -b $new_branch || return
+    git rebase "$remote/main"
 end
