2f368a18f7612903677e705619b0e3c576a1a873
Justus Perlwitz <hello@justus.pw>
2024-02-07 11:14:27 +0900

    Fix alacritty path in aerospace
    The second one shoulda have been from pkgs too

--- nix/home-manager/aerospace.nix
+++ nix/home-manager/aerospace.nix
@@ -2,9 +2,10 @@
 { lib, pkgs }:
 let
   source = builtins.fromTOML (builtins.readFile ../../aerospace/aerospace.toml);
+  alacritty = "${pkgs.alacritty}/bin/alacritty";
   extra = {
     mode.main.binding = {
-      alt-enter = "exec-and-forget ${pkgs.alacritty}/bin/alacritty msg create-window || alacritty";
+      alt-enter = "exec-and-forget ${alacritty} msg create-window || ${alacritty}";
     };
   };
   merged = lib.attrsets.recursiveUpdate source extra;
