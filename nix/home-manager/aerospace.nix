de93dcd87c995c67339ef0fa250c31b364d94509
Justus Perlwitz <hello@justus.pw>
2024-02-09 09:49:39 +0900

    Create sway alt-shift-m equivalent for aerospace

--- nix/home-manager/aerospace.nix
+++ nix/home-manager/aerospace.nix
@@ -3,11 +3,18 @@
 let
   source = builtins.fromTOML (builtins.readFile ../../aerospace/aerospace.toml);
   alacritty = "${pkgs.alacritty}/bin/alacritty";
+  # Hardcoded woops
+  firefox = "/Applications/Free/Firefox.app/Contents/MacOS/firefox";
+  fish = "${pkgs.fish}/bin/fish";
   extra = {
     mode.main.binding = {
       alt-enter = "exec-and-forget ${alacritty} msg create-window || ${alacritty}";
-      # Hardcoded woops
-      alt-shift-enter = "exec-and-forget /Applications/Free/Firefox.app/Contents/MacOS/firefox --new-window";
+      alt-shift-enter = "exec-and-forget ${firefox} --new-window";
+      alt-shift-m = [
+        "workspace 4"
+        "exec-and-forget ${alacritty} msg create-window -e ${fish} -l -c manage-dotfiles"
+        "exec-and-forget ${firefox} --new-window"
+      ];
     };
   };
   merged = lib.attrsets.recursiveUpdate source extra;
