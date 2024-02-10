a341819e35531ff2b5b877acf49562ccddf88e9e
Justus Perlwitz <hello@justus.pw>
2024-02-10 17:49:55 +0900

    Add projectify launcher in fish

---
+++ fish/functions/projectify.fish
@@ -0,0 +1,43 @@
+function projectify
+    # This is a relatively complete example of how to create a complex session in
+    # tmux using nothing but tmux commands
+    set session projectify
+    set projectify_path "$HOME/projects/projectify/"
+
+    # Attach if already created
+    if tmux has-session -t $session
+        tsa $session
+        return
+    end
+
+    # Editor for frontend
+    tmux new-session -c "$projectify_path/projectify-frontend" -d -s $session -n frontend
+    tmux send-keys -t "$session:frontend" nvim C-m
+
+    tmux split-window -c "$projectify_path/projectify-frontend" -h -t "$session:frontend"
+    tmux send-keys -t "$session:frontend" git status C-m
+
+    # Serve frontend and storybook
+    tmux new-window -c "$projectify_path/projectify-frontend" -t $session -n frontend-serve
+    tmux send-keys -t "$session:frontend-serve" "npm run dev" C-m
+
+    tmux split-window -c "$projectify_path/projectify-frontend" -v -t "$session:frontend-serve"
+    tmux send-keys -t "$session:frontend-serve" "npm run storybook" C-m
+
+    # Editor for backend
+    tmux new-window -c "$projectify_path/projectify-backend" -t $session -n backend
+    tmux send-keys -t "$session:backend" "poetry run nvim" C-m
+
+    tmux split-window -c "$projectify_path/projectify-backend" -h -t "$session:backend"
+    tmux send-keys -t "$session:backend" "git status" C-m
+
+    # Serve backend django server
+    tmux new-window -c "$projectify_path/projectify-backend" -t $session -n backend-serve
+    tmux send-keys -t "$session:backend-serve" "poetry run ./manage.py runserver" C-m
+
+    # Go to first window
+    tmux select-window -t "$session:frontend"
+
+    # Attach
+    tsa $session
+end
