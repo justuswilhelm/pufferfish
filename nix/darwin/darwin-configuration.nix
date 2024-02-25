534e8c2b4bf5f021782ec5ac67a189f95704a8c3
Justus Perlwitz <hello@justus.pw>
2024-02-25 09:30:02 +0900

    Make AeroSpace auto-start using nix darwin config

--- nix/darwin/darwin-configuration.nix
+++ nix/darwin/darwin-configuration.nix
@@ -3,6 +3,7 @@ let
   uid = 501;
   home = "/Users/${name}";
   library = "${home}/Library";
+  nixApplications = "${home}/Applications/Home Manager Apps";
 in
 { user, config, pkgs, ... }:
 
@@ -116,6 +117,15 @@ in
           # Time out after 3 minutes
           TimeOut = 3 * 60;
         };
+    };
+    "aerospace" = {
+      serviceConfig = {
+        ProgramArguments = [
+          "${nixApplications}/AeroSpace.app/Contents/MacOS/AeroSpace"
+          "--started-at-login"
+        ];
+        RunAtLoad = true;
+      };
     };
   };
 
