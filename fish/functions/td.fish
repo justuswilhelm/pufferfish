e0933dc29319c6b8025ea5dc8d26710bfab17faa
Justus Perlwitz <hello@justus.pw>
2024-05-24 16:51:30 +0200

    Fish: Make td fn fail-safe
    Don't try to start a new session when directory is not provided.
    This can happen when pressing ctrl+c while fzf runs

--- fish/functions/td.fish
+++ fish/functions/td.fish
@@ -1,7 +1,19 @@
 function td -d "Create a new tmux session in a given directory"
     set hist_file $XDG_STATE_HOME/pufferfish/td.hist
-    mkdir -vp (dirname $hist_file)
-    set dir (fd -t d | fzf --scheme=history --history=$hist_file)
-    set session_name (basename $dir)
-    tmux new-session -c $dir -s $session_name
+    if ! mkdir -vp (dirname $hist_file)
+        echo "Couldn't make directory for hist_file $hist_file"
+        return 1
+    end
+    if ! set dir (fd -t d | fzf --scheme=history --history=$hist_file)
+        echo "Couldn't determine new tmux session directory"
+        return 1
+    end
+    if ! set session_name (basename $dir)
+        echo "Couldn't determine new tmux session name"
+        return 1
+    end
+    if ! tmux new-session -c $dir -s $session_name
+        echo "Couldn't create new tmux session $session_name in directory $dir"
+        return 1
+    end
 end
