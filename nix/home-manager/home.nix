9473db027ebb3328fa073ce89ea31601c3a1a328
Justus Perlwitz <hello@justus.pw>
2024-06-04 14:36:12 +0900

    Switch from pdbpp config to pdb

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -83,25 +83,42 @@ in
       '';
       target = "${applicationSupport}/pypoetry/config.toml";
     };
-    pdbpp = {
-      text = ''
-        import pdb
-
-        class Config(pdb.DefaultConfig):
-            def setup(self, Pdb: pdb.Pdb) -> None:
+    pdbrc =
+      let
+        pdbrcpy = pkgs.writeTextFile {
+          name = "pdbrc.py";
+          # Might want to create the following folder:
+          # $HOME/.local/state/pdb/pdb_history
+          text = ''
+            import pdb
+            def _pdbrc_init() -> None:
                 import readline as r
+                import pathlib
                 from atexit import register
-
-                register(r.write_history_file, ".pdb_history")
-
+                history_d = pathlib.Path.home() / ".local/state/pdb"
+                history_f = history_d / "pdb_history"
+                if not history_d.exists():
+                  history_d.mkdir(parents=True, exist_ok=True)
+                register(r.write_history_file, history_f)
                 try:
-                    r.read_history_file(".pdb_history")
+                    r.read_history_file(history_f)
                 except IOError:
                     pass
                 r.set_history_length(1000)
-      '';
-      target = ".pdbrc.py";
-    };
+            _pdbrc_init()
+            del _pdbrc_init
+          '';
+        };
+      in
+      {
+        text = ''
+          import os
+          with open(os.path.expanduser("${pdbrcpy}")) as _f: _f = _f.read()
+          exec(_f)
+          del _f
+        '';
+        target = ".pdbrc";
+      };
   };
 
   programs.home-manager.enable = true;
