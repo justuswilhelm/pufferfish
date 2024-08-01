3e241733ac947a9d5a90e4caa93eba78f6c9d676
Justus Perlwitz <hello@justus.pw>
2024-08-01 15:34:53 +0900

    Nix/NixOS/Lithium: Configure qemu/utm support

--- nix/nixos/lithium-nixos/configuration.nix
+++ nix/nixos/lithium-nixos/configuration.nix
@@ -7,11 +7,12 @@
 {
   imports =
     [
-      # Include the results of the hardware scan.
-      ./hardware-configuration.nix
       ../networkd.nix
       ../yubikey.nix
       ../sway.nix
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./utm.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
@@ -44,7 +45,6 @@
     nnn
 
     #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    spice-vdagent
     i3status
     # spice-autorandr
     #   wget
@@ -61,30 +61,6 @@
       experimental-features = nix-command flakes
     '';
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
