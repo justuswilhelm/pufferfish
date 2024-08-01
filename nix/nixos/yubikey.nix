ee383799cbff6299cd4b3898ee84510164c54d27
Justus Perlwitz <hello@justus.pw>
2024-08-01 06:31:51 +0000

    Nix/NixOS: Enable u2f pam for yubikey

--- nix/nixos/yubikey.nix
+++ nix/nixos/yubikey.nix
@@ -4,5 +4,9 @@
   services.openssh.enable = true;
 
   services.pcscd.enable = true;
-
+  # https://nixos.wiki/wiki/Yubikey
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
+  };
 }
