c00b41a00e8e3842455cc612a5754819c65a475e
Justus Perlwitz <hello@justus.pw>
2024-08-01 16:01:40 +0900

    Nix/NixOS: Fix swaylock pam

--- nix/nixos/yubikey.nix
+++ nix/nixos/yubikey.nix
@@ -4,9 +4,22 @@
   services.openssh.enable = true;
 
   services.pcscd.enable = true;
+
   # https://nixos.wiki/wiki/Yubikey
+  # Add key cfg
+  # mkdir -p $HOME/.config/Yubico
+  # pamu2fcfg > $HOME/.config/Yubico/u2f_keys
+  # Test all three:
+  # pamtester login $USER authenticate
+  # pamtester sudo $USER authenticate
+  # pamtester swaylock $USER authenticate
   security.pam.services = {
     login.u2fAuth = true;
     sudo.u2fAuth = true;
+    swaylock.u2fAuth = true;
   };
+  environment.systemPackages = [
+    pkgs.pam_u2f
+    pkgs.pamtester
+  ];
 }
