5a546c524d1e775c8d1cf04a65c5024eff031b15
Justus Perlwitz <hello@justus.pw>
2024-08-30 17:01:25 +0900

    NixOS: Add db for infosec

--- nix/nixos/modules/infosec.nix
+++ nix/nixos/modules/infosec.nix
@@ -13,4 +13,30 @@ in
     isNormalUser = true;
     home = "/tmp/${user}";
   };
+  users.groups.msf = { };
+  users.users.msf = {
+    description = "user for Metasploit db";
+    group = "msf";
+    isSystemUser = true;
+  };
+
+  # DB for Metasploit
+  # Inside Metasplot, run
+  # msfdb init --connection-string postgresql://msf@msf?host=/var/run/postgresql
+  # db_connect msf@localhost/msf
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ "msf" ];
+    ensureUsers = [{
+      name = "msf";
+      ensureDBOwnership = true;
+    }];
+    authentication = ''
+      local msf all peer map=msf
+      host msf all 127.0.0.1/32 trust
+    '';
+    identMap = ''
+      msf /.+ msf
+    '';
+  };
 }
