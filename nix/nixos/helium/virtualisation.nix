0c4f0b0cf4da7a261dff42fec946d131655c4b70
Justus Perlwitz <hello@justus.pw>
2024-09-13 10:45:18 +0900

    NixOS/Helium: Fix lint

--- nix/nixos/helium/virtualisation.nix
+++ nix/nixos/helium/virtualisation.nix
@@ -7,7 +7,9 @@
   # Thanks to:
   # https://astrid.tech/2022/09/22/0/nixos-gpu-vfio/
   boot.initrd.availableKernelModules = [
-    "vfio_pci" "vfio" "vfio_iommu_type1"
+    "vfio_pci"
+    "vfio"
+    "vfio_iommu_type1"
     # https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=e2d55709398e62cf53e5c7df3758ae52cc62d63a
     # virqfd not needed anymore:
     # "vfio_virqfd"
