e6135b94718d0f58c10908a0accb89af4b96a132
Justus Perlwitz <hello@justus.pw>
2024-09-17 07:58:30 +0900

    NixOS/Helium: Enable ime

--- nix/nixos/helium/configuration.nix
+++ nix/nixos/helium/configuration.nix
@@ -9,6 +9,7 @@
       ../modules/openvpn.nix
       ../modules/borgmatic.nix
       ../modules/infosec.nix
+      ../modules/ime.nix
 
       # TODO set up impermanence
       # https://github.com/nix-community/impermanence
