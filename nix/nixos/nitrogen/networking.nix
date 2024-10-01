e4399a43de645ab1201b38c541aa5b689477b501
Justus Perlwitz <hello@justus.pw>
2024-10-01 09:40:28 +0900

    NixOS: Fix nitrogen network config

--- nix/nixos/nitrogen/networking.nix
+++ nix/nixos/nitrogen/networking.nix
@@ -3,19 +3,9 @@
   networking.hostName = "nitrogen"; # Define your hostname.
   # Pick only one of the below networking options.
   networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
-  networking.wireless.environmentFile = "/run/secrets/wireless.env";
+  networking.wireless.environmentFile = "/etc/wireless-credentials/wireless.env";
   networking.wireless.networks = {
     "@HOME_SSID@".psk = "@HOME_PSK@";
   };
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking.firewall.enable = true;
 }
