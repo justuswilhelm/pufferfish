15e032bdb80c71a7f53b6be14502e4e7aa4fa6ae
Justus Perlwitz <hello@justus.pw>
2024-10-01 10:29:01 +0900

    NixOS: Configure man pages

---
+++ nix/nixos/modules/man.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }:
+{
+  documentation.dev.enable = true;
+  documentation.man = {
+    enable = true;
+    generateCaches = true;
+    man-db.enable = false;
+    mandoc.enable = true;
+  };
+  environment.systemPackages = [
+    pkgs.man-pages
+    pkgs.man-pages-posix
+  ];
+}
