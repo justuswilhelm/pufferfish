5928de6c20dccabb808932fa3c689a0bb5d0cc86
Justus Perlwitz <hello@justus.pw>
2024-10-03 09:53:35 +0900

    Fish: Make t function default to PWD for name

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,9 +1,15 @@
 function t -d "Create a new tmux session with the given name" -a name
     if [ -n "$name" ]
-        set session $name
-    else if ! read -P "New tmux session name: " session
-        echo "No session name specified. Exiting."
+        set session_name $name
+    else
+        echo "Assuming session name is current directory name"
+        set session_name (basename $PWD)
+    end
+    if ! tmux new-session -d -s $session_name
+        echo "Couldn't create new tmux session $session_name"
         return 1
     end
-    tmux new-session -s $session
+    if ! tsa $session_name
+        echo "Couldn't tsa to session $session_name"
+    end
 end
