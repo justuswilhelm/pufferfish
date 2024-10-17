349efd9050d05ea5035086c71bc7a246e2be3398
Justus Perlwitz <hello@justus.pw>
2024-10-17 10:24:05 +0900

    Nix/Home: Increase SSH privacy

--- nix/home-manager/modules/ssh.nix
+++ nix/home-manager/modules/ssh.nix
@@ -6,6 +6,11 @@
     extraOptionOverrides = {
       IdentitiesOnly = "yes";
       IdentityFile = "/dev/null";
+      # Avoid leaking our current user name
+      User = "root";
+      # Avoid being prompted for password. Re-enable if needed for specific
+      # hosts
+      PasswordAuthentication = "no";
     };
   };
 }
