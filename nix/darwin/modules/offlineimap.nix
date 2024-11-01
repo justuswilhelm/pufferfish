12448196d53d3c185add1a00c0a69cc793f39497
Justus Perlwitz <hello@justus.pw>
2024-11-01 17:14:48 +0900

    Nix/Darwin: Add passive monitoring to offlineimap

--- nix/darwin/modules/offlineimap.nix
+++ nix/darwin/modules/offlineimap.nix
@@ -9,13 +9,19 @@ in
 {
   launchd.user.agents = {
     "offlineimap" = {
+      path = [ offlineimap config.services.nagios.nsca-package ];
+      script = ''
+        if offlineimap -l '${logPath}/offlineimap.log'; then
+          echo -e 'lithium.local,offlineimap,0,success' | send_nsca 127.0.0.1 -p 5667 -c /etc/nagios/send_nsca.conf -d ,
+        else
+          echo -e 'lithium.local,offlineimap,2,fail' | send_nsca 127.0.0.1 -p 5667 -c /etc/nagios/send_nsca.conf -d ,
+        fi
+      '';
       serviceConfig = {
-        ProgramArguments = [
-          "${offlineimap}/bin/offlineimap"
-          "-l"
-          "/dev/stdout"
-        ];
-        StandardOutPath = "${logPath}/offlineimap.log";
+        # Performance
+        ProcessType = "Background";
+        LowPriorityBackgroundIO = true;
+        LowPriorityIO = true;
         # Every 5 minutes
         StartInterval = 5 * 60;
         # Time out after 3 minutes
