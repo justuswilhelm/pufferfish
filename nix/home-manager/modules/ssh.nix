57b30625e673c1ae20b31c672884d6b44f6b40b2
Justus Perlwitz <hello@justus.pw>
2024-11-24 18:34:00 +0900

    Ssh: Disable /dev/null'ing identity file

--- nix/home-manager/modules/ssh.nix
+++ nix/home-manager/modules/ssh.nix
@@ -5,7 +5,6 @@
     addKeysToAgent = "yes";
     extraOptionOverrides = {
       IdentitiesOnly = "yes";
-      IdentityFile = "/dev/null";
       # Avoid leaking our current user name
       # User = "root";
       # Avoid being prompted for password. Re-enable if needed for specific
