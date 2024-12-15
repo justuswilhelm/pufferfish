05ce429f7f1e20130572c5a9bd8423a444d052f7
Justus Perlwitz <hello@justus.pw>
2024-12-16 06:56:20 +0900

    Nix/Darwin: Refactor anki config

--- nix/darwin/anki.nix
+++ nix/darwin/anki.nix
@@ -1,26 +1,35 @@
+# TODO add password hashing
 { pkgs, ... }:
 let
-  anki-sync-serer = pkgs.anki-sync-server;
+  anki-sync-server = pkgs.anki-sync-server;
+  # TODO
+  # homePath = "/var/lib/anki-sync-server";
+  # TODO make this
+  # statePath = "${homePath}/sync";
+  statePath = "/var/anki-sync-server";
   logPath = "/var/log/anki-sync-server";
+  # TODO make this
+  # syncUser1 = "${statePath}/users/sync_user_1";
+  syncUser1 = "/etc/anki-sync-server/sync_user1";
 in
 {
   users.groups.anki-sync-server = {
     gid = 601;
   };
   users.users.anki-sync-server = {
+    # TODO remove createHome and add home path
+    # inherit homePath;
     createHome = false;
     description = "Anki-sync-server user";
     gid = 601;
     uid = 51;
     isHidden = true;
   };
-  users.knownGroups = [
-    "anki-sync-server"
-  ];
-  users.knownUsers = [
-    "anki-sync-server"
-  ];
+  users.knownGroups = [ "anki-sync-server" ];
+  users.knownUsers = [ "anki-sync-server" ];
+
   launchd.daemons.anki-sync-server = {
+    path = [ anki-sync-server ];
     script = ''
       SYNC_USER1="$(cat /etc/anki-sync-server/sync_user1)"
       export SYNC_USER1
@@ -32,7 +41,7 @@ in
       # > const pbkdf2 = require("@phc/pbkdf2");
       # console.log(await pbkdf2.hash("password");
       # undefined
-      exec ${anki-sync-serer}/bin/anki-sync-server
+      exec anki-sync-server
     '';
     serviceConfig = {
       KeepAlive = true;
@@ -42,17 +51,18 @@ in
       EnvironmentVariables = {
         SYNC_HOST = "127.0.0.1";
         SYNC_PORT = "18090";
-        SYNC_BASE = "/var/anki-sync-server";
+        SYNC_BASE = statePath;
       };
     };
   };
-  system.activationScripts.postActivation = {
+  system.activationScripts.preActivation = {
     text = ''
-      mkdir -vp /var/log/anki-sync-server
-      mkdir -vp /var/anki-sync-server
+      mkdir -vp ${logPath}
+      mkdir -vp ${statePath}
+      chown -R anki-sync-server:anki-sync-server ${statePath} /etc/anki-sync-server ${logPath}
+
       mkdir -vp /etc/anki-sync-server
-      chown -R anki-sync-server:anki-sync-server /var/anki-sync-server /etc/anki-sync-server /var/log/anki-sync-server
-      chmod -v 0400 /etc/anki-sync-server/sync_user1
+      chmod -v 0400 ${syncUser1}
     '';
   };
 }
