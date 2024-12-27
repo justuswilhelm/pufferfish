da512cf707e2c1458b7aedbcc1774f1118b8f21f
Justus Perlwitz <hello@justus.pw>
2024-12-27 13:46:50 +0900

    Nix: Disable verbose home manager build

--- nix/generic/flake.nix
+++ nix/generic/flake.nix
@@ -126,7 +126,6 @@
             home-manager.darwinModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
-              home-manager.verbose = true;
               home-manager.useUserPackages = true;
               home-manager.sharedModules = [
                 { _module.args = inputs; }
