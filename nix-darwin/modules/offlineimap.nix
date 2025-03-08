600236683bf8899162437ce8be2889a3336f6d3d
Justus Perlwitz <hello@justus.pw>
2025-03-08 18:19:51 +0900

    Nix-Darwin: Fix offlineimap timeout

--- nix-darwin/modules/offlineimap.nix
+++ nix-darwin/modules/offlineimap.nix
@@ -5,13 +5,14 @@ let
   home = "/Users/${name}";
   library = "${home}/Library";
   logPath = "${library}/Logs/offlineimap";
+  timeout = 3 * 60;
 in
 {
   launchd.user.agents = {
     "offlineimap" = {
       path = [ offlineimap config.services.nagios.nsca-package ];
       script = ''
-        if offlineimap -l '${logPath}/offlineimap.log'; then
+        if timeout --signal INT ${toString timeout} offlineimap -l '${logPath}/offlineimap.log'; then
           echo -e 'lithium.local,offlineimap,0,success' | send_nsca 127.0.0.1 -p 5667 -c /etc/nagios/send_nsca.conf -d ,
         else
           echo -e 'lithium.local,offlineimap,2,fail' | send_nsca 127.0.0.1 -p 5667 -c /etc/nagios/send_nsca.conf -d ,
@@ -24,8 +25,6 @@ in
         LowPriorityIO = true;
         # Every 5 minutes
         StartInterval = 5 * 60;
-        # Time out after 3 minutes
-        TimeOut = 3 * 60;
       };
     };
   };
