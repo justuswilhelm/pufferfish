d342dad18e1872a9b3ce3d02da9ce569a1268b04
Justus Perlwitz <hello@justus.pw>
2025-03-08 21:49:34 +0900

    Nix-Darwin: Simplify nix config

--- nix-darwin/modules/nix.nix
+++ nix-darwin/modules/nix.nix
@@ -1,13 +1,10 @@
 { config, pkgs, ... }:
 {
-  nix.nixPath = [
-    "/nix/var/nix/profiles/per-user/root/channels"
-  ];
+  nix.nixPath = [ "/nix/var/nix/profiles/per-user/root/channels" ];
   nix.extraOptions = ''
     experimental-features = flakes nix-command
   '';
   nix.settings = {
-    trusted-users = [ "root" ];
     sandbox = true;
     extra-sandbox-paths = [ "/nix/store" ];
   };
