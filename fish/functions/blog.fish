15aa8a54b3c3d011556d17cba0ba7435a0069a7f
Justus Perlwitz <hello@justus.pw>
2025-03-11 11:22:11 +0900

    Fish: Update blog.fish function

--- fish/functions/blog.fish
+++ fish/functions/blog.fish
@@ -7,10 +7,17 @@ function blog
         tsa $session
         return
     end
-    tmux new-session -c $cwd -d -s $session
-    tmux send-keys -t $session "nvim" C-m
+
+    tmux new-session -c $cwd -d -s $session -n editor
+    tmux send-keys -t $session:editor "nvim" C-m
     tmux split-window -c $cwd -h -t $session
-    tmux send-keys -t $session "git status" C-m
+    tmux send-keys -t $session:editor "git status" C-m
+
+    tmux new-window -c $cwd -t $session -n hugo_preview
+    tmux send-keys -t $session:hugo_preview 'hugo server -D --destination (mktemp -d)' C-m
+
+    # Go to first window
+    tmux select-window -t $session:editor
 
     tsa $session
 end
