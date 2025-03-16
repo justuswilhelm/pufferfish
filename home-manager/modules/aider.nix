dc6af55c02a3916b33ea238122c5e64b9acfb77a
Justus Perlwitz <hello@justus.pw>
2025-03-16 12:04:45 +0900

    Home: Update aider config

--- home-manager/modules/aider.nix
+++ home-manager/modules/aider.nix
@@ -1,10 +1,18 @@
 { lib, pkgs, config, options, ... }:
+let
+  yamlFormat = pkgs.formats.yaml { };
+  config = {
+    # https://aider.chat/docs/leaderboards/
+    model = "anthropic/claude-3-7-sonnet-20250219";
+    auto-commits = false;
+    light-mode = true;
+    # Yay, we can enable git again
+    # https://aider.chat/HISTORY.html#aider-v0760
+    # Fixed Git identity retrieval to respect global configuration, by Akira Komamura.
+    git = true;
+  };
+in
 {
-  home.file.".aider.conf.yml".text = ''
-    haiku: true
-    auto-commits: false
-    light-mode: true
-    git: false
-  '';
+  home.file.".aider.conf.yml".source = yamlFormat.generate ".aider.conf.yml" config;
   programs.git.ignores = [ ".aider*" ];
 }
