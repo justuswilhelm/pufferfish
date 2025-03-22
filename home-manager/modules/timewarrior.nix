483d7afdaf3e5d6857c89a7d21bf6d5c36bc73be
Justus Perlwitz <hello@justus.pw>
2025-03-22 17:43:09 +0900

    Home: Refactor timewarrior cfg

--- home-manager/modules/timewarrior.nix
+++ home-manager/modules/timewarrior.nix
@@ -1,10 +1,5 @@
 { pkgs, ... }:
 {
-  xdg.configFile.timewarrior = {
-    source = ../../timewarrior/timewarrior.cfg;
-    target = "timewarrior/timewarrior.cfg";
-  };
-  home.packages = [
-    pkgs.timewarrior
-  ];
+  xdg.configFile."timewarrior/timewarrior.cfg".source = ../../timewarrior/timewarrior.cfg;
+  home.packages = [ pkgs.timewarrior ];
 }
