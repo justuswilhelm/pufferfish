dc42bdd053342ae046f33e8f6a9c404bddf2d5b3
Justus Perlwitz <hello@justus.pw>
2025-03-22 18:08:08 +0900

    Home: Refactor neomutt.nix

--- home-manager/modules/neomutt.nix
+++ home-manager/modules/neomutt.nix
@@ -11,6 +11,17 @@ let
         wrapProgram $out/bin/neomutt --set TERM xterm-direct
       '';
     };
+  convert = pkgs.writeShellApplication {
+    name = "convert";
+    runtimeInputs = with pkgs; [ pandoc libuchardet ];
+    text = ''
+      format="$(uchardet "$1")"
+      if [ "$format" = "unknown" ]; then
+        format="utf-8"
+      fi
+      iconv -f "$format" -t utf-8 "$1" | pandoc -f html -t plain -
+    '';
+  };
 in
 {
   programs.neomutt = {
@@ -21,29 +32,9 @@ in
   };
 
   xdg.configFile = {
-    neomuttShared = {
-      source = ../../neomutt/shared;
-      target = "neomutt/shared";
-    };
-    neomuttMailcap = {
-      text =
-        let
-          convert = pkgs.writeShellApplication {
-            name = "convert";
-            runtimeInputs = with pkgs; [ pandoc libuchardet ];
-            text = ''
-              format="$(uchardet "$1")"
-              if [ "$format" = "unknown" ]; then
-                format="utf-8"
-              fi
-              iconv -f "$format" -t utf-8 "$1" | pandoc -f html -t plain -
-            '';
-          };
-        in
-        ''
-          text/html; ${convert}/bin/convert %s; copiousoutput
-        '';
-      target = "neomutt/mailcap";
-    };
+    "neomutt/shared".source = ../../neomutt/shared;
+    "neomutt/mailcap".text = ''
+      text/html; ${convert}/bin/convert %s; copiousoutput
+    '';
   };
 }
