bafec6fe5f37832b31ad4459f35ee1c4f5256a07
Justus Perlwitz <hello@justus.pw>
2025-06-09 09:35:44 +0900

    Nix-Darwin: Update security hardening settings

--- nix-darwin/modules/security.nix
+++ nix-darwin/modules/security.nix
@@ -3,13 +3,18 @@
   # https://github.com/LnL7/nix-darwin/issues/165#issuecomment-1256957157
   # For iterm2 see:
   # https://apple.stackexchange.com/questions/259093/can-touch-id-on-mac-authenticate-sudo-in-terminal/355880#355880
-  security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local = {
+    reattach = true;
+    touchIdAuth = true;
+  };
 
   power.sleep.computer = 3;
   power.sleep.display = 3;
   system.defaults = {
     loginwindow = {
       GuestEnabled = false;
+      SHOWFULLNAME = true;
+      DisableConsoleAccess = true;
     };
     screensaver.askForPassword = true;
     screensaver.askForPasswordDelay = null;
