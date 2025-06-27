de2220a94515ac4d629a542c507bdbc4542720ea
Justus Perlwitz <hello@justus.pw>
2025-06-27 09:02:07 +0900

    Nix-Darwin Refactor caddy service cfg

--- nix-darwin/modules/caddy.nix
+++ nix-darwin/modules/caddy.nix
@@ -220,6 +220,22 @@ in
       };
     };
 
+    services.nagios.objectDefs =
+      let
+        host = "localhost";
+        port = 2019;
+        nagiosCfg = pkgs.writeText "caddy.cfg" ''
+          define service {
+              use generic-service
+              host_name ${host}
+              service_description Caddy
+              display_name Caddy web server
+              check_command check_curl!-p ${toString port} --expect='HTTP/1.1 404'
+          }
+        '';
+      in
+      lib.optional config.services.nagios.enable nagiosCfg;
+
     environment.systemPackages = [ caddy ];
 
     launchd.daemons.caddy = {
