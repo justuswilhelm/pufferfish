da6abb2356972650e6a171f68b7750286972c178
Justus Perlwitz <hello@justus.pw>
2025-07-24 20:11:01 +0900

    Nix-Darwin: Imrpove sync-git-annex

--- nix-darwin/modules/sync-git-annex.nix
+++ nix-darwin/modules/sync-git-annex.nix
@@ -4,6 +4,7 @@ with lib;
 
 let
   cfg = config.services.sync-git-annex;
+  logPath = "/Users/${config.system.primaryUser}/Library/Logs/sync-git-annex/sync-git-annex.log";
 in
 {
   options.services.sync-git-annex = {
@@ -12,14 +13,26 @@ in
 
   # should be a user agent
   config = mkIf cfg.enable {
+    services.newsyslog.modules.sync-git-annex = {
+      ${logPath} = {
+        mode = "640";
+        count = 10;
+        size = "*";
+        when = "$D0";
+        flags = "J";
+      };
+    };
+
     launchd.daemons.sync-git-annex = {
-      path = [ pkgs.fish ];
+    # TODO consider adding timeout
       script = ''
-        fish -l sync-git-annex
+        /Users/${config.system.primaryUser}/.dotfiles/bin/sync-git-annex --non-interactive \
+          2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
       '';
       serviceConfig = {
         StartCalendarInterval = [{ Minute = 0; }];
         UserName = config.system.primaryUser;
+        StandardOutPath = logPath;
       };
     };
   };
