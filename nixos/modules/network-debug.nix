61c30ad6c277f078e307da61a2a14e56cd420b12
Justus Perlwitz <hello@justus.pw>
2025-08-26 16:46:48 +0900

    NixOS: Improve traceroute behavior
    Allowlist with OpenSnitch, make suid

--- nixos/modules/network-debug.nix
+++ nixos/modules/network-debug.nix
@@ -1,5 +1,5 @@
 # Modules used to debug networking
-{ specialArgs, pkgs, ... }:
+{ specialArgs, pkgs, lib, ... }:
 {
   environment.systemPackages = with pkgs; [
     tcpdump
@@ -13,12 +13,50 @@
 
     kismet
   ];
+
+  programs.traceroute.enable = true;
   programs.wireshark.enable = true;
   programs.tcpdump.enable = true;
+
   users.users.${specialArgs.name}.extraGroups = [
     # For tcpdump
     "pcap"
     # For wireshark
     "wireshark"
   ];
+
+  services.opensnitch.rules = {
+    traceroute-udp = {
+      name = "Allow traceroute UDP";
+      created = "1970-01-01T00:00:00Z";
+      updated = "1970-01-01T00:00:00Z";
+      enabled = true;
+      action = "allow";
+      duration = "always";
+      operator = {
+        type = "list";
+        operand = "list";
+        list = [
+          { type = "simple"; operand = "process.path"; data = "${lib.getBin pkgs.traceroute}/bin/traceroute"; }
+          { type = "simple"; operand = "protocol"; data = "udp"; }
+        ];
+      };
+    };
+    traceroute-udp6 = {
+      name = "Allow traceroute UDP6";
+      created = "1970-01-01T00:00:00Z";
+      updated = "1970-01-01T00:00:00Z";
+      enabled = true;
+      action = "allow";
+      duration = "always";
+      operator = {
+        type = "list";
+        operand = "list";
+        list = [
+          { type = "simple"; operand = "process.path"; data = "${lib.getBin pkgs.traceroute}/bin/traceroute"; }
+          { type = "simple"; operand = "protocol"; data = "udp6"; }
+        ];
+      };
+    };
+  };
 }
