15215023d53a97b4e4a773c9d53a9f105709924f
Justus Perlwitz <hello@justus.pw>
2025-08-27 19:24:13 +0900

    Neovim: Define new code fence paste cmd

---
+++ nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+-- Paste clipboard contents with code fence
+vim.keymap.set(
+    "n",
+    "<leader>cfp",
+    -- 1. Write code fence. Mark.
+    -- 2. Paste
+    -- 3. Write closing code fence. Jump to mark.
+    "o```<esc>magpo```<esc>`a"
+)
