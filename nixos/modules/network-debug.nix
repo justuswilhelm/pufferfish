f2c8b1c2c7db651930545934e4ccc3a46aee8968
Justus Perlwitz <hello@justus.pw>
2025-09-04 13:58:43 +0900

    NixOS: Fix network-debug firewall rules

--- nixos/modules/network-debug.nix
+++ nixos/modules/network-debug.nix
@@ -25,38 +25,28 @@
     "wireshark"
   ];
 
-  services.opensnitch.rules = {
-    traceroute-udp = {
-      name = "Allow traceroute UDP";
-      created = "1970-01-01T00:00:00Z";
-      updated = "1970-01-01T00:00:00Z";
-      enabled = true;
-      action = "allow";
-      duration = "always";
-      operator = {
-        type = "list";
-        operand = "list";
-        list = [
-          { type = "simple"; operand = "process.path"; data = "${lib.getBin pkgs.traceroute}/bin/traceroute"; }
-          { type = "simple"; operand = "protocol"; data = "udp"; }
-        ];
+  services.opensnitch.rules =
+    let
+      tracerouteProtocols = [ "udp" "udp6" "icmp" ];
+      makeTracerouteRule = protocol: {
+        name = "Allow traceroute ${protocol}";
+        created = "1970-01-01T00:00:00Z";
+        updated = "1970-01-01T00:00:00Z";
+        enabled = true;
+        action = "allow";
+        duration = "always";
+        operator = {
+          type = "list";
+          operand = "list";
+          list = [
+            { type = "regexp"; operand = "process.path"; data = "^${lib.getBin pkgs.traceroute}/bin/traceroute$|^traceroute$"; }
+            { type = "simple"; operand = "protocol"; data = protocol; }
+          ];
+        };
       };
-    };
-    traceroute-udp6 = {
-      name = "Allow traceroute UDP6";
-      created = "1970-01-01T00:00:00Z";
-      updated = "1970-01-01T00:00:00Z";
-      enabled = true;
-      action = "allow";
-      duration = "always";
-      operator = {
-        type = "list";
-        operand = "list";
-        list = [
-          { type = "simple"; operand = "process.path"; data = "${lib.getBin pkgs.traceroute}/bin/traceroute"; }
-          { type = "simple"; operand = "protocol"; data = "udp6"; }
-        ];
-      };
-    };
-  };
+    in
+    lib.listToAttrs (map (protocol: {
+      name = "traceroute-${protocol}";
+      value = makeTracerouteRule protocol;
+    }) tracerouteProtocols);
 }
