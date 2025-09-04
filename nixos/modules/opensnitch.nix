dcefc80f5579b66c0c95e98dd7aae2768b1b9dc7
Justus Perlwitz <hello@justus.pw>
2025-09-04 14:39:46 +0900

    NixOS: Refactor firefox fw cfg

--- nixos/modules/opensnitch.nix
+++ nixos/modules/opensnitch.nix
@@ -1,8 +1,4 @@
 { lib, pkgs, config, ... }:
-let
-  firefox = config.programs.firefox.package;
-  firefoxBin = "${firefox}/lib/firefox";
-in
 {
   # https://nixos.wiki/wiki/OpenSnitch
   # https://github.com/evilsocket/opensnitch/wiki/Rules
@@ -40,71 +36,6 @@ in
             { type = "simple"; operand = "process.path"; data = "${lib.getBin pkgs.systemd}/lib/systemd/systemd-resolved"; }
             { type = "simple"; operand = "protocol"; data = "udp"; }
             { type = "regexp"; operand = "dest.port"; data = "^(53|5353)$"; }
-          ];
-        };
-      };
-      forbid-mozilla = {
-        name = "Forbid *.mozilla.* domains that provide no user benefit";
-        created = "1970-01-01T00:00:00Z";
-        updated = "1970-01-01T00:00:00Z";
-        enabled = true;
-        action = "reject";
-        duration = "always";
-        precedence = true;
-        operator = {
-          type = "regexp";
-          sensitive = false;
-          operand = "dest.host";
-          data = "[^.]+\.services\.mozilla\.com";
-        };
-      };
-      firefox-allow-443 = {
-        name = "Allow Firefox connections to port 443";
-        created = "1970-01-01T00:00:00Z";
-        updated = "1970-01-01T00:00:00Z";
-        enabled = true;
-        action = "allow";
-        duration = "always";
-        operator = {
-          type = "list";
-          operand = "list";
-          list = [
-            { type = "regexp"; operand = "process.path"; data = firefoxBin; }
-            { type = "simple"; operand = "dest.port"; data = "443"; }
-          ];
-        };
-      };
-      firefox-allow-local-80 = {
-        name = "Allow local Firefox connections to :80";
-        created = "1970-01-01T00:00:00Z";
-        updated = "1970-01-01T00:00:00Z";
-        enabled = true;
-        action = "allow";
-        duration = "always";
-        precedence = true;
-        operator = {
-          type = "list";
-          operand = "list";
-          list = [
-            { type = "regexp"; operand = "process.path"; data = firefoxBin; }
-            { type = "simple"; operand = "dest.port"; data = "80"; }
-            { type = "network"; operand = "dest.network"; data = "10.0.0.0/8"; }
-          ];
-        };
-      };
-      firefox-deny-80 = {
-        name = "Deny Firefox connections to :80";
-        created = "1970-01-01T00:00:00Z";
-        updated = "1970-01-01T00:00:00Z";
-        enabled = true;
-        action = "reject";
-        duration = "always";
-        operator = {
-          type = "list";
-          operand = "list";
-          list = [
-            { type = "regexp"; operand = "process.path"; data = firefoxBin; }
-            { type = "simple"; operand = "dest.port"; data = "80"; }
           ];
         };
       };
