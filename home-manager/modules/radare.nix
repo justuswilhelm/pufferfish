de5083760d167bb94c2e6da9d7cf56ccc9061d16
Justus Perlwitz <hello@justus.pw>
2025-09-05 16:13:39 +0900

    Home: Refactor radare config

--- home-manager/modules/radare.nix
+++ home-manager/modules/radare.nix
@@ -1,25 +1,16 @@
 { pkgs, lib, config, ... }:
 let
   cfg = config.programs.radare2;
+  radare = pkgs.symlinkJoin {
+    name = "radare2";
+    paths = [ pkgs.radare2 pkgs.meson pkgs.ninja ];
+  };
 in
 {
   options = {
     programs.radare2.enable = lib.mkEnableOption "Install radare2";
   };
   config = {
-    home.packages = [
-      # Reverse engineering
-      # ===================
-      (
-        pkgs.symlinkJoin {
-          name = "radare2";
-          paths = [
-            pkgs.radare2
-            pkgs.meson
-            pkgs.ninja
-          ];
-        }
-      )
-    ];
+    home.packages = [ radare ];
   };
 }
