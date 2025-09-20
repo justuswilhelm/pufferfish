828e3a0020afe33bd660b2eb6bf0774ed515da9b
Justus Perlwitz <hello@justus.pw>
2025-09-20 16:13:59 +0900

    Cmus: Reorganize config

--- home-manager/modules/cmus.nix
+++ home-manager/modules/cmus.nix
@@ -16,6 +16,11 @@ in
   config = mkIf cfg.enable {
     programs.cmus.extraConfig = ''
       set output_plugin=${cfg.output_plugin}
+      set lib_sort=genre artist
+
+      # https://manpages.ubuntu.com/manpages/trusty/man1/cmus.1.html
+      set format_playlist=%{genre} %{artist} %-15%{album} %3{tracknumber}. %{title}%= %{date} %{duration}
+
       ${builtins.readFile ../../cmus/rc}
     '';
   };
