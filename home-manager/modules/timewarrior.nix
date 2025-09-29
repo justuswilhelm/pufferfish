a9c9d264c95ba36f2d764cf5070f1283b13d3766
Justus Perlwitz <hello@justus.pw>
2025-09-29 18:35:45 +0900

    Home: Fix timew alias

--- home-manager/modules/timewarrior.nix
+++ home-manager/modules/timewarrior.nix
@@ -6,5 +6,5 @@
 {
   xdg.configFile."timewarrior/timewarrior.cfg".source = ../../timewarrior/timewarrior.cfg;
   home.packages = [ pkgs.timewarrior ];
-  programs.fish.shellAliases.tw = "timewarrior";
+  programs.fish.shellAliases.tw = "timew";
 }
