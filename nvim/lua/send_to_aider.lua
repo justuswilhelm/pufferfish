a665adab62e414c274b56fa6cd34735caf3bdee9
Justus Perlwitz <hello@justus.pw>
2025-10-09 13:43:11 +0900

    Neovim: Update send_to_aider

--- nvim/lua/send_to_aider.lua
+++ nvim/lua/send_to_aider.lua
@@ -9,37 +9,19 @@ local M = {}
 -- Define patterns to match for aider panes
 M.AIDER_PATTERNS = {"aid", "aider"}
 
--- Get the current buffer's path relative to Git root
+-- Get the current buffer's path relative to the current working directory
 function M.get_relative_path()
-    local git_root = vim.fs.root(0, '.git')
-    if not git_root then
-        vim.notify("Couldn't determine root. Are you inside a Git repository?",
-                   vim.log.levels.ERROR)
-        return
-    end
+    -- This always returns a string
+    local cwd = vim.fn.getcwd()
 
+    -- This always returns a name
     local current_buffer = vim.api.nvim_buf_get_name(0)
-    if not current_buffer then
-        vim.notify("Couldn't determine current buffer path",
-                   vim.log.levels.ERROR)
-        return
-    end
 
+    -- This always returns an absolute path
     local current_buffer_abs = vim.fs.abspath(current_buffer)
-    if not current_buffer_abs then
-        vim.notify(string.format(
-                       "Couldn't determine absolute path of current buffer %s",
-                       current_buffer), vim.log.levels.ERROR)
-        return
-    end
 
-    local relative_path = vim.fs.relpath(git_root, current_buffer_abs)
-    if not relative_path then
-        vim.notify(string.format("Couldn't retrieve path of %s relative to %s",
-                                 current_buffer_abs, git_root),
-                   vim.log.levels.ERROR)
-        return
-    end
+    -- This always returns a relative path
+    local relative_path = vim.fs.relpath(cwd, current_buffer_abs)
 
     return relative_path
 end
@@ -82,7 +64,6 @@ function M.send_aider_command(text)
                 break
             end
         end
-        if pane_id then break end
     end
 
     if not pane_id then
@@ -135,24 +116,18 @@ end
 -- Function to add current buffer path to aider
 function M.add_to_aider()
     local relative_path = M.get_relative_path()
-    if not relative_path then return end
-
     M.send_aider_command(string.format("/add %s", relative_path))
 end
 
 -- Function to add current buffer path to aider, read-only
 function M.add_to_aider_read_only()
     local relative_path = M.get_relative_path()
-    if not relative_path then return end
-
     M.send_aider_command(string.format("/read-only %s", relative_path))
 end
 
 -- Function to drop current buffer path from aider
 function M.drop_from_aider()
     local relative_path = M.get_relative_path()
-    if not relative_path then return end
-
     M.send_aider_command(string.format("/drop %s", relative_path))
 end
 
