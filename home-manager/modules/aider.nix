953811ad2b82888fc5b705ef827ce3863cd4bd16
Justus Perlwitz <hello@justus.pw>
2026-02-02 11:10:50 +0900

    Home: Replace aider with nixpkgs aider

--- home-manager/modules/aider.nix
+++ home-manager/modules/aider.nix
@@ -31,20 +31,6 @@ in
   programs.git.ignores = [ ".aider*" ];
 
   home.packages = [
-    pkgs.pipx
-    (pkgs.writeShellApplication {
-      name = "aid";
-      runtimeInputs = lib.optionals isLinux [
-        pkgs.stdenv.cc.cc.lib
-        pkgs.glibc
-        pkgs.ungoogled-chromium
-        pkgs.bash
-      ];
-      text = ''
-        ${lib.optionalString isLinux ''export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib64:${pkgs.stdenv.cc.cc.lib}/lib:$${LD_LIBRARY_PATH:-}"''}
-        export SHELL=bash
-        pipx run aider-chat "$@"
-      '';
-    })
+    pkgs.aider-chat-with-playwright
   ];
 }
