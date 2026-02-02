cf7331dee26323f71e44fa14d29cc31e572511c3
Justus Perlwitz <hello@justus.pw>
2026-02-02 11:10:10 +0900

    Nix-Darwin: Install updated ngn-k

--- nix-darwin/modules/overlays.nix
+++ nix-darwin/modules/overlays.nix
@@ -11,6 +11,21 @@
     # Make J build again on darwin
     (final: previous: {
       j = previous.j.overrideAttrs (final: previous: { meta.broken = false; });
+      ngn-k = previous.ngn-k.overrideAttrs (oldAttrs: {
+
+        src = previous.fetchFromGitea {
+          domain = "codeberg.org";
+          owner = "growler";
+          repo = "k";
+          rev = "e066c4b4b89832d690abebe3973744ccdf6e3bcb";
+          sha256 = "sha256-hN5H35S/Zy9sW9yyBuDs0QWvtxvdAqavQAXE1EFzemc=";
+        };
+  patches = [
+    ./repl-license-path.patch
+    ./repl-argv-1.patch
+  ];
+        meta.platforms = oldAttrs.meta.platforms ++ [ "aarch64-darwin" ];
+      });
     })
   ];
 }
