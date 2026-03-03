620517255108cd2213413aac8fb6723c62f61bb7
Justus Perlwitz <hello@justus.pw>
2026-03-03 14:43:16 +0900

    Fish: Update projectify function

--- fish/functions/projectify.fish
+++ fish/functions/projectify.fish
@@ -14,24 +14,25 @@ function projectify --description "Launch a Projectify tmux session"
         return
     end
     # Editor for backend
-    tmux new-window -c "$projectify_path/backend" -t $session -n backend
-    tmux send-keys -t "$session:backend" "poetry run nvim" C-m
 
-    tmux split-window -c "$projectify_path/backend" -h -t "$session:backend"
-    tmux send-keys -t "$session:backend" "git status" C-m
+    tmux new-session -c "$projectify_path" -d -s $session -n django
+    tmux send-keys -t "$session:django" "uv run nvim" C-m
+
+    tmux split-window -c "$projectify_path" -h -t "$session:django"
+    tmux send-keys -t "$session:django" "git status" C-m
 
     # Serve backend django server
-    tmux new-window -c "$projectify_path/backend" -t $session -n backend-serve
-    tmux send-keys -t "$session:backend-serve" "poetry run ./manage.py runserver" C-m
-    tmux split-window -c "$projectify_path/backend" -v -t "$session:backend-serve"
-    tmux send-keys -t "$session:backend-serve" "poetry run ./manage.py tailwind start" C-m
+    tmux new-window -c "$projectify_path" -t $session -n django-serve
+    tmux send-keys -t "$session:django-serve" "uv run ./manage.py runserver" C-m
+    tmux split-window -c "$projectify_path" -v -t "$session:django-serve"
+    tmux send-keys -t "$session:django-serve" "npm run dev" C-m
 
     # Open root folder
     tmux new-window -c "$projectify_path" -t $session -n shell
     tmux send-keys -t "$session:shell" "git status" C-m
 
     # Go to first window
-    tmux select-window -t "$session:backend"
+    tmux select-window -t "$session:django"
 
     # Attach
     tsa $session
