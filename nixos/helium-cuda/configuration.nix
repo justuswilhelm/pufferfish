f69e336580bdfab04e6eb7d9eec4ee7e3dedbcee
Justus Perlwitz <hello@justus.pw>
2026-06-07 13:28:13 +0900

    NixOS/helium-cuda: Remove nixos cuda cache

--- nixos/helium-cuda/configuration.nix
+++ nixos/helium-cuda/configuration.nix
@@ -64,15 +64,6 @@
   };
   services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
 
-  nix.settings = {
-    substituters = [
-      "https://cache.nixos-cuda.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-    ];
-  };
-
   programs.git.enable = true;
   programs.neovim.enable = true;
   environment.systemPackages = [
