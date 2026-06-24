d521eb015d8632fce2f378da9771232ab1498fc5
Justus Perlwitz <hello@justus.pw>
2026-06-24 10:53:41 +0900

    Home/Alacritty: Enable fish shell

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-{ lib, ... }:
+{ config, lib, ... }:
 {
   programs.alacritty = {
     enable = true;
@@ -12,6 +12,12 @@
         normal = {
           family = "Iosevka Fixed";
         };
+      };
+      # TODO check if we can check if nixosConfig.users.users.${specialArgs.name} has
+      # enabled fish
+      terminal.shell = lib.mkIf config.programs.fish.enable {
+        program = "${config.programs.fish.package}/bin/fish";
+        args = [ "-l" ];
       };
       window = {
         option_as_alt = "OnlyRight";
