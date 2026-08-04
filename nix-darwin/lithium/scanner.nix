6c54863ef2dd8090be918dac681ebc5b4f76fe3b
Justus Perlwitz <hello@justus.pw>
2026-08-04 14:15:09 +0900

    Nix-Darwin: Improve scanner.nix

--- nix-darwin/lithium/scanner.nix
+++ nix-darwin/lithium/scanner.nix
@@ -10,6 +10,9 @@
 let
   logPath = "/Users/${config.system.primaryUser}/Library/Logs/scansnap/scansnap.log";
   scanSnapApp = "/Applications/ScanSnapHomeMain.app";
+  attachScript = pkgs.writeShellScript "scansnap-attach" ''
+    /usr/bin/open -a "${scanSnapApp}"
+  '';
   # See https://stackoverflow.com/questions/13987671/launchd-plist-runs-every-10-seconds-instead-of-just-once/49902760#49902760:~:text=gcc%20%2Dframework%20Foundation%20%2Do%20xpc%5Fset%5Fevent%5Fstream%5Fhandler%20xpc%5Fset%5Fevent%5Fstream%5Fhandler%2Em
   xpcHandler = pkgs.runCommandCC "xpc_set_event_stream_handler" { } ''
     mkdir $out $out/bin
@@ -31,20 +34,23 @@ in
 
   launchd.user.agents.scansnap-ix1300-attach = {
     path = [ pkgs.moreutils ];
-    script = ''
-      (
-        exec ${xpcHandler}/bin/xpc_set_event_stream_handler \
-          /usr/bin/open -a ${scanSnapApp}
-      ) 2>&1 | ts '[%Y-%m-%d %H:%M:%S]'
-    '';
     serviceConfig = {
+      ProgramArguments = [
+        "${xpcHandler}/bin/xpc_set_event_stream_handler"
+        "${attachScript}"
+      ];
       StandardOutPath = logPath;
-      KeepAlive = false;
+      StandardErrorPath = logPath;
+      # Default is false
+      # KeepAlive = false;
       # See
       # https://github.com/snosrap/xpc_set_event_stream_handler
       LaunchEvents = {
         "com.apple.iokit.matching" = {
-          "com.apple.device-attach" = {
+          # > The value for this key is the string that was given as the
+          # > name for the event in the launchd.plist(5).
+          # https://keith.github.io/xcode-man-pages/xpc_events.3.html#EVENT_CONSUMPTION
+          "ix1300" = {
             # Run this to find the IDs:
             # system_profiler SPUSBDataType | grep ScanSnap -A20
             idProduct = lib.trivial.fromHexString "162c";
--- nix-darwin/lithium/xpc_set_event_stream_handler.m
+++ nix-darwin/lithium/xpc_set_event_stream_handler.m
@@ -1,21 +1,30 @@
 // SPDX-FileCopyrightText: 2017 Ford Parsons
 // SPDX-License-Identifier: CC-BY-SA-4.0
 // Found on https://stackoverflow.com/a/49902760
+// Doesn't support detach events because launchd's iokit integration
+// only sends events on device attach
 
 #import <Foundation/Foundation.h>
 #include <xpc/xpc.h>
 
 int main(int argc, const char * argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <on-attach-program>\n",
+argv[0]);
+        return 1;
+    }
+
+    NSLog(@"Started with: %s", argv[1]);
+
     @autoreleasepool {
         dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
         xpc_set_event_stream_handler("com.apple.iokit.matching", NULL, ^(xpc_object_t _Nonnull object) {
             const char *event = xpc_dictionary_get_string(object, XPC_EVENT_KEY_NAME);
-            NSLog(@"%s", event);
+            NSLog(@"Event: %s", event);
             dispatch_semaphore_signal(semaphore);
         });
+        NSLog(@"Waiting for event...");
         dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
-        if(argc >= 2) {
-            execv(argv[1], (char **)argv+1);
-        }
+        return system(argv[1]);
     }
 }
